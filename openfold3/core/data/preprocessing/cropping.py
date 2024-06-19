4a2cb0186ca8ac821923470d91bbd30c0fb511d4
gnikolenyi <nikolenyi.gergo@gmail.com>
2024-06-19 14:43:37 -0400

    updated environment with pl-upgrades and biotite==0.41.0

--- openfold3/core/data/preprocessing/cropping.py
+++ openfold3/core/data/preprocessing/cropping.py
@@ -1,2 +1,9 @@
 # TODO add license
 
+# Some biotite examples
+from tempfile import gettempdir
+import biotite.database.rcsb as rcsb
+import biotite.structure.io.pdbx as pdbx
+
+cif_file_path = rcsb.fetch("1l2y", "cif", gettempdir())
+cif_file = pdbx.PDBxFile.read(cif_file_path)
