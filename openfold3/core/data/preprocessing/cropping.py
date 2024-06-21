348cd45cdfa31bb419d86ebe521bc3ca7ccd8fcb
gnikolenyi <nikolenyi.gergo@gmail.com>
2024-06-20 22:19:23 -0400

    initial croppinh, tokenization and chain assignment

--- openfold3/core/data/preprocessing/cropping.py
+++ openfold3/core/data/preprocessing/cropping.py
@@ -1,12 +1,15 @@
 # TODO add license
 
 # Some biotite examples
-
+#%%
 import biotite.database.rcsb as rcsb
 import biotite.structure as struc
 import biotite.structure.io.pdbx as pdbx
 import numpy as np
+from numpy.random import Generator
 from biotite.structure import AtomArray
+from openfold3.core.data.preprocessing.tokenization import tokenize_atomarray, chain_assignment
+
 
 # Protein trimer with non-covalent ligands
 pdbx_file_8FAQ = pdbx.BinaryCIFFile.read(rcsb.fetch("8FAQ", "bcif"))
@@ -16,11 +19,6 @@ pdbx_file_1NVP = pdbx.BinaryCIFFile.read(rcsb.fetch("1NVP", "bcif"))
 pdbx_file_3US4 = pdbx.BinaryCIFFile.read(rcsb.fetch("3US4", "bcif"))
 # Protein RNA complex
 pdbx_file_1A9N = pdbx.BinaryCIFFile.read(rcsb.fetch("1A9N", "bcif"))
-
-
-pdbx.get_sequence(pdbx_file_8FAQ)
-pdbx.get_sequence(pdbx_file_1NVP)
-pdbx.get_sequence(pdbx_file_3US4)
 
 biounit_8FAQ = pdbx.get_assembly(pdbx_file_8FAQ, assembly_id="1", model=1)
 biounit_1NVP = pdbx.get_assembly(pdbx_file_1NVP, assembly_id="1", model=1)
@@ -33,36 +31,79 @@ biounit_1NVP_noHOH = biounit_1NVP[biounit_1NVP.res_name != "HOH"]
 biounit_3US4_noHOH = biounit_3US4[biounit_3US4.res_name != "HOH"]
 biounit_1A9N_noHOH = biounit_1A9N[biounit_1A9N.res_name != "HOH"]
 
+#%%
+biounit_8FAQ_noHOH = chain_assignment(tokenize_atomarray(biounit_8FAQ_noHOH))
+biounit_1NVP_noHOH = chain_assignment(tokenize_atomarray(biounit_1NVP_noHOH))
+biounit_3US4_noHOH = chain_assignment(tokenize_atomarray(biounit_3US4_noHOH))
+biounit_1A9N_noHOH = chain_assignment(tokenize_atomarray(biounit_1A9N_noHOH))
 
+atomarray = biounit_1A9N_noHOH
+generator = np.random.default_rng(2346)
+n_res = 384
+chains = np.array(list(set(atomarray.af3_chain_id)), dtype=int)
+chains = generator.permutation(chains)
+print(chains)
+#%%
 # print("Number of chains:", struc.get_chain_count(biounit_3US4))
 # struc.get_residues(biounit_3US4_noHOH)
 
 # strucio.save_structure("/mnt/c/Users/nikol/Documents/biotite_tests/1NVP_noHOH.pdb", biounit_1NVP_noHOH)
 
 
-def get_token_start_ids(atomarray: AtomArray):
-    return
-
-
-def contiguous_crop(atomarray: AtomArray, n_res: int, seed: int):
-    """Implements Contiguous Cropping Algorithm 1 from AF-Multimer section 7.2.1.
+def contiguous_crop(atomarray: AtomArray, n_res: int, generator: Generator):
+    """Implements Contiguous Cropping, Algorithm 1 from AF-Multimer section 7.2.1.
 
     Args:
         atomarray (AtomArray): biotite atom array of the first bioassembly of a PDB entry
-        n_res (int): residue budget i.e. crop size
-        seed (int): seed used for chain permutation and crop sampling
+        n_res (int): residue budget i.e. total crop size
+        generator (Generator): a numpy generator set with a specific seed
     """
-    # Create numpy generator
-    generator = np.random.default_rng(seed)
 
-    # Get chain starts and lengths
-    chain_start_ids = struc.get_chain_starts(atomarray)
-    chain_lens = 0
+    # Get chain ids and permute
+    chains = np.array(list(set(atomarray.af3_chain_id)), dtype=int)
+    chains = generator.permutation(chains)
 
-    n_chains = len(chain_start_ids)
+    # Create cropping mask annotation
+    atomarray.set_annotation("af3_contiguous_crop_mask", np.repeat(False, len(atomarray)))
+
+    # Cropping loop
     n_added = 0
     n_remaining = n_res
-    for chain_start_id_k in chain_start_ids:
-        n_remaining -= 0
+    for chain_id in chains:
 
+        # TODO need to deal with atomic tokens including:
+        # ligands - separate chain from polymer
+        # ligands - same chain as poly
+        # covanlently modified residues
+
+        atomarray_chain = atomarray[atomarray.af3_chain_id == chain_id]
+        n_k = struc.get_residue_count(atomarray_chain)
+        n_remaining -= n_k
+
+        # Calculate crop length
+        crop_size_max = np.min([n_res - n_added, n_k])
+        crop_size_min = np.min([n_k, np.max([0, n_res - n_added - n_remaining])])
+        crop_size = generator.integers(crop_size_min, crop_size_max + 1, 1).item()
+        n_added += crop_size
+
+        # Calculate crop start
+        crop_start = generator.integers(0, n_k - crop_size + 1, 1).item()
+
+        # Create mask - could also edit the AtomArray annotation directly but looks much less clean
+        m_k = np.zeros(n_k, dtype=bool)
+        m_k[crop_start:crop_start + crop_size] = True
+
+        # Get atom indices of residue starting points for residues in crop
+        residue_starts = struc.get_residue_starts(atomarray_chain)
+        residue_starts_crop = residue_starts[crop_start:crop_start + crop_size]
+
+
+
+    return
+
+def spatial_crop():
+    return
+
+
+def spatial_interface_crop():
     return
--- openfold3/core/data/preprocessing/tokenization.py
+++ openfold3/core/data/preprocessing/tokenization.py
@@ -3,7 +3,7 @@ import numpy as np
 from biotite.structure import AtomArray
 
 # Standard residues as defined in AF3 SI, Table 13
-STANDARD_RESIDUES = [
+STANDARD_PROTEIN_RESIDUES = [
     "ALA",
     "ARG",
     "ASN",
@@ -25,6 +25,8 @@ STANDARD_RESIDUES = [
     "TYR",
     "VAL",
     "UNK",
+]
+STANDARD_NUCLEIC_ACID_RESIDUES = [
     "A",
     "G",
     "C",
@@ -36,8 +38,10 @@ STANDARD_RESIDUES = [
     "N",
     "DN",
 ]
+STANDARD_RESIDUES = STANDARD_PROTEIN_RESIDUES + STANDARD_NUCLEIC_ACID_RESIDUES
 
-TOKEN_CENTER_ATOM_NAMES = ["CA", "C1'"]
+# Token center atoms as defined in AF3 SI, Section 2.6.
+TOKEN_CENTER_ATOMS = ["CA", "C1'"]
 
 
 def tokenize_atomarray(atomarray: AtomArray):
@@ -79,8 +83,51 @@ def tokenize_atomarray(atomarray: AtomArray):
     # Create token center atom annotation
     af3_token_center_atoms = np.repeat(True, n_atoms)
     af3_token_center_atoms[is_standard_residue_atom] = np.isin(
-        atomarray[is_standard_residue_atom].atom_name, TOKEN_CENTER_ATOM_NAMES
+        atomarray[is_standard_residue_atom].atom_name, TOKEN_CENTER_ATOMS
     )
     atomarray.set_annotation("af3_token_center_atom", af3_token_center_atoms)
 
+    return atomarray
+
+
+def chain_assignment(atomarray: AtomArray):
+    """Generate chain ids and molecule types for a biotite AtomArray.
+
+    Separate chain ids are given to each protein chain, nucleic acid chain and
+    non-covalent ligands including lipids, glycans and small molecules. TODO Covalently
+    bound ligands are assigned the id of the chain they are bound to if they have
+    less than n atoms, otherwise they are assigned a separate chain id.
+
+    Args:
+        atomarray (AtomArray): biotite atom array of the first bioassembly of a PDB entry
+
+    Returns:
+        AtomArray: biotite atom array with added 'af3_chain_id' and 'af3_molecule_type' annotations
+    """
+
+    # TODO For now, only consider separate chains as assigned by biotite (disregard covalent ligands etc)
+    # Get chain id start indices
+    n_atoms = len(atomarray)
+    chain_start_ids = struc.get_chain_starts(atomarray)
+
+    # Create chain ids
+    chain_id_repeats = np.diff(np.append(chain_start_ids, n_atoms))
+    chain_ids_per_atom = np.repeat(np.arange(len(chain_id_repeats)), chain_id_repeats)
+    atomarray.set_annotation("af3_chain_id", chain_ids_per_atom)
+
+    # Create molecule type annotation
+    molecule_types = np.zeros(len(atomarray))
+    for start_id, end_id in zip(chain_start_ids, chain_start_ids + chain_id_repeats):
+        residues_in_chain = set(atomarray[start_id:end_id].res_name)
+        # Assign protein
+        if residues_in_chain & set(STANDARD_PROTEIN_RESIDUES):
+            molecule_types[start_id:end_id] = 0
+        # Assign nucleic acid
+        elif residues_in_chain & set(STANDARD_NUCLEIC_ACID_RESIDUES):
+            molecule_types[start_id:end_id] = 1
+        # Assign ligand
+        else:
+            molecule_types[start_id:end_id] = 2
+    # TODO need to add annotation for covalently modified residues as they are tokenized atomically
+    atomarray.set_annotation("af3_molecule_type", molecule_types)
     return atomarray
