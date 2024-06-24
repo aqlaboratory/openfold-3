c065f6ad2929b3c1ddb75f8a8b0d998292b5843d
gnikolenyi <nikolenyi.gergo@gmail.com>
2024-06-24 19:24:47 -0400

    crop_contiguous 1st version

--- openfold3/core/data/preprocessing/cropping.py
+++ openfold3/core/data/preprocessing/cropping.py
@@ -1,19 +1,24 @@
 # TODO add license
 
 # Some biotite examples
-#%%
+# %%
 import biotite.database.rcsb as rcsb
 import biotite.structure as struc
 import biotite.structure.io.pdbx as pdbx
 import numpy as np
 from numpy.random import Generator
 from biotite.structure import AtomArray
-from openfold3.core.data.preprocessing.tokenization import tokenize_atom_array, chain_assignment
+from openfold3.core.data.preprocessing.tokenization import (
+    tokenize_atom_array,
+    assign_chains,
+)
 
 
 # Protein trimer with covalent and non-covalent glycans
-pdbx_file_8FAQ = pdbx.CIFFile.read(rcsb.fetch("8FAQ", "cif"), )
-# Protein trimer with non-removed ions and non-covalent ligands 
+pdbx_file_8FAQ = pdbx.CIFFile.read(
+    rcsb.fetch("8FAQ", "cif"),
+)
+# Protein trimer with non-removed ions and non-covalent ligands
 pdbx_file_1PCR = pdbx.CIFFile.read(rcsb.fetch("1PCR", "cif"))
 # Protein-DNA complex
 pdbx_file_1NVP = pdbx.CIFFile.read(rcsb.fetch("1NVP", "cif"))
@@ -22,11 +27,46 @@ pdbx_file_3US4 = pdbx.CIFFile.read(rcsb.fetch("3US4", "cif"))
 # Protein RNA complex
 pdbx_file_1A9N = pdbx.CIFFile.read(rcsb.fetch("1A9N", "cif"))
 
-biounit_8FAQ = pdbx.get_assembly(pdbx_file_8FAQ, assembly_id="1", model=1, altloc='occupancy', use_author_fields=False, include_bonds=True)
-biounit_1PCR = pdbx.get_assembly(pdbx_file_1PCR, assembly_id="1", model=1, altloc='occupancy', use_author_fields=False, include_bonds=True)
-biounit_1NVP = pdbx.get_assembly(pdbx_file_1NVP, assembly_id="1", model=1, altloc='occupancy', use_author_fields=False, include_bonds=True)
-biounit_3US4 = pdbx.get_assembly(pdbx_file_3US4, assembly_id="1", model=1, altloc='occupancy', use_author_fields=False, include_bonds=True)
-biounit_1A9N = pdbx.get_assembly(pdbx_file_1A9N, assembly_id="1", model=1, altloc='occupancy', use_author_fields=False, include_bonds=True)
+biounit_8FAQ = pdbx.get_assembly(
+    pdbx_file_8FAQ,
+    assembly_id="1",
+    model=1,
+    altloc="occupancy",
+    use_author_fields=False,
+    include_bonds=True,
+)
+biounit_1PCR = pdbx.get_assembly(
+    pdbx_file_1PCR,
+    assembly_id="1",
+    model=1,
+    altloc="occupancy",
+    use_author_fields=False,
+    include_bonds=True,
+)
+biounit_1NVP = pdbx.get_assembly(
+    pdbx_file_1NVP,
+    assembly_id="1",
+    model=1,
+    altloc="occupancy",
+    use_author_fields=False,
+    include_bonds=True,
+)
+biounit_3US4 = pdbx.get_assembly(
+    pdbx_file_3US4,
+    assembly_id="1",
+    model=1,
+    altloc="occupancy",
+    use_author_fields=False,
+    include_bonds=True,
+)
+biounit_1A9N = pdbx.get_assembly(
+    pdbx_file_1A9N,
+    assembly_id="1",
+    model=1,
+    altloc="occupancy",
+    use_author_fields=False,
+    include_bonds=True,
+)
 
 # Remove waters
 biounit_8FAQ = biounit_8FAQ[biounit_8FAQ.res_name != "HOH"]
@@ -35,32 +75,32 @@ biounit_1NVP = biounit_1NVP[biounit_1NVP.res_name != "HOH"]
 biounit_3US4 = biounit_3US4[biounit_3US4.res_name != "HOH"]
 biounit_1A9N = biounit_1A9N[biounit_1A9N.res_name != "HOH"]
 
-#%%
+# %%
 tokenize_atom_array(biounit_8FAQ)
 tokenize_atom_array(biounit_1PCR)
 tokenize_atom_array(biounit_1NVP)
 tokenize_atom_array(biounit_3US4)
 tokenize_atom_array(biounit_1A9N)
-chain_assignment(biounit_8FAQ)
-chain_assignment(biounit_1PCR)
-chain_assignment(biounit_1NVP)
-chain_assignment(biounit_3US4)
-chain_assignment(biounit_1A9N)
+assign_chains(biounit_8FAQ)
+assign_chains(biounit_1PCR)
+assign_chains(biounit_1NVP)
+assign_chains(biounit_3US4)
+assign_chains(biounit_1A9N)
 
-atom_array = biounit_1A9N
+atom_array = biounit_8FAQ
 generator = np.random.default_rng(2346)
-n_res = 384
+token_budget = 384
 chains = np.array(list(set(atom_array.af3_chain_id)), dtype=int)
 chains = generator.permutation(chains)
 print(chains)
-#%%
+# %%
 # print("Number of chains:", struc.get_chain_count(biounit_3US4))
 # struc.get_residues(biounit_3US4)
 
 # strucio.save_structure("/mnt/c/Users/nikol/Documents/biotite_tests/1NVP.pdb", biounit_1NVP)
 
 
-def crop_contiguous(atom_array: AtomArray, n_res: int, generator: Generator):
+def crop_contiguous(atom_array: AtomArray, token_budget: int, generator: Generator):
     """Implements Contiguous Cropping, Algorithm 1 from AF-Multimer section 7.2.1.
 
     Updates the input biotite atom array with added 'af3_crop_mask' annotation in-place.
@@ -68,9 +108,9 @@ def crop_contiguous(atom_array: AtomArray, n_res: int, generator: Generator):
     Args:
         atom_array (atom_array):
             biotite atom array of the first bioassembly of a PDB entry
-        n_res (int):
-            residue budget i.e. total crop size
-        generator (Generator): 
+        token_budget (int):
+            token budget i.e. total crop size
+        generator (Generator):
             a numpy generator set with a specific seed
 
     Returns:
@@ -85,38 +125,41 @@ def crop_contiguous(atom_array: AtomArray, n_res: int, generator: Generator):
     atom_array.set_annotation("af3_crop_mask", np.repeat(False, len(atom_array)))
 
     # Cropping loop
-    n_added = 0
-    n_remaining = n_res
+    tokens_added = 0
+    tokens_remaining = token_budget
     for chain_id in chains:
+
         # Get chain atom array and type
         atom_array_chain = atom_array[atom_array.af3_chain_id == chain_id]
-        molecule_type_chain = set(atom_array_chain.af3_molecule_type) == 0
 
-        # If standard polymer WITHOUT covalent modifications
-        if (molecule_type_chain == 0) | (molecule_type_chain == 1):
-            n_k = struc.get_residue_count(atom_array_chain)
-        # TODO If standard polymer WITH covalent modifications
-        # If nonstandard residue in polymer or ligand
-        else:
-            n_k = len(atom_array_chain)
-        n_remaining -= n_k
+        # Get chain length
+        chain_length = atom_array_chain.af3_token_id[-1] - atom_array_chain.af3_token_id[0] + 1
+
+        # Cannot have negative number of tokens remaining
+        tokens_remaining = max(tokens_remaining - chain_length, 0)
 
         # Calculate crop length
-        crop_size_max = np.min([n_res - n_added, n_k])
-        crop_size_min = np.min([n_k, np.max([0, n_res - n_added - n_remaining])])
+        crop_size_max = np.min([token_budget - tokens_added, chain_length])
+        crop_size_min = np.min([chain_length, np.max([0, token_budget - tokens_added - tokens_remaining])])
         crop_size = generator.integers(crop_size_min, crop_size_max + 1, 1).item()
-        n_added += crop_size
+
+        tokens_added += crop_size
 
         # Calculate crop start and map to global atom index
-        crop_start = generator.integers(0, n_k - crop_size + 1, 1).item()
+        crop_start = generator.integers(0, chain_length - crop_size + 1, 1).item()
         crop_start_global = atom_array_chain[crop_start].af3_atom_id
 
         # Edit corresponding segment in crop mask
-        atom_array.af3_crop_mask[crop_start_global:crop_start_global + crop_size] = True
+        atom_array.af3_crop_mask[crop_start_global : crop_start_global + crop_size] = (
+            True
+        )
 
-
+        # Break if allocated all of the token budget
+        if tokens_remaining == 0:
+            break
 
     return None
+
 
 def crop_spatial():
     return
