f05d119cea03645a42287b3b11193083f52bc5f9
gnikolenyi <nikolenyi.gergo@gmail.com>
2024-06-27 19:23:39 -0400

    revert to data_pipeline branch versions for files not supposed to be in this commit, snake case formatting

--- openfold3/core/data/data_structures/data_module.py
+++ openfold3/core/data/data_structures/data_module.py
@@ -6,28 +6,26 @@ from typing import Dict, List, Sequence
 
 import pytorch_lightning as pl
 import torch
-
 from torch.utils.data import DataLoader
-from openfold3.base.data.data_structures.singledatasets import (
+
+from openfold3.core.data.data_structures.single_datasets import (
     DATASET_REGISTRY,
     OpenFoldSingleDataset,
 )
-from openfold3.base.data.data_structures.stochasticsamplerdataset import (
+from openfold3.core.data.data_structures.stochastic_sampler_dataset import (
     OpenFoldStochasticSamplerDataset,
 )
-from openfold3.base.utils.tensor_utils import dict_multimap
+from openfold3.core.utils.tensor_utils import dict_multimap
 
 
 class OpenFoldDataModule(pl.LightningDataModule):
     def __init__(self, data_config: List[Sequence[Dict]]) -> None:
         super().__init__()
 
-        # Input argument self-assignment
-        self.batch_size = data_config["batch_size"]
-        self.num_workers = data_config["num_workers"]
-        self.data_seed = data_config["data_seed"]
-        self.virtual_epoch_len = data_config["virtual_epoch_len"]
-        self.num_virtual_epochs = data_config["num_virtual_epochs"]
+        # TODO Input argument self-assignment - only assign necessary attributes if any
+
+        # TODO Argument checks
+        # train/validation/test/predict datasets exclusivity
 
         # Parse data_config
         dataset_classes, dataset_weights, dataset_configs, dataset_types = (
@@ -36,26 +34,28 @@ class OpenFoldDataModule(pl.LightningDataModule):
 
         # Initialize datasets
         # QUESTION do we want to support validation/testing/prediction on multiple datasets?
-        if ("train" in dataset_types) | ("valid" in dataset_types):
+        if ("train" in dataset_types) | ("validation" in dataset_types):
             # Initialize train datasets
             train_datasets = self.init_datasets(
                 dataset_classes, dataset_configs, dataset_types, "train"
             )
 
-            self.generator = torch.Generator(device="cpu").manual_seed(self.data_seed)
+            generator = torch.Generator().manual_seed(
+                "<data_seed>"
+            )  # TODO add argument
 
             # Wrap train datasets in the sampler dataset class
             self.train_dataset = OpenFoldStochasticSamplerDataset(
                 datasets=train_datasets,
                 probabilities=dataset_weights,
-                virtual_epoch_len=self.virtual_epoch_len,
-                num_virtual_epochs=self.num_virtual_epochs,
-                generator=self.generator,
+                virtual_epoch_len="<virtual_epoch_len from data config>",  # TODO add argument
+                num_virtual_epochs="<num_virtual_epochs from data config>",  # TODO add argument
+                generator=generator,
             )
 
-            # Currently only one valid dataset is supported
-            self.valid_dataset = self.init_datasets(
-                dataset_classes, dataset_configs, dataset_types, "valid"
+            # Currently only one validation dataset is supported
+            self.validation_dataset = self.init_datasets(
+                dataset_classes, dataset_configs, dataset_types, "validation"
             )[0]
 
         elif "test" in dataset_types:
@@ -99,17 +99,6 @@ class OpenFoldDataModule(pl.LightningDataModule):
                 ]
             )
         )
-        dataset_types_unique = set(dataset_types)
-        if (len(dataset_types_unique) == 2) & (
-            ("train" not in dataset_types) | ("valid" not in dataset_types)
-        ):
-            raise ValueError(
-                f"An unsupported combination of dataset types was found in data_config: {dataset_types_unique}. The supported dataset combinations are: ['train'], ['train', 'valid'], ['test'], ['predict']."
-            )
-        elif (len(dataset_types_unique) == 1) & ("valid" in dataset_types):
-            raise ValueError(
-                "Validation dataset(s) were provided without any training datasets. The supported dataset combinations are: ['train'], ['train', 'valid'], ['test'], ['predict']."
-            )
 
         return dataset_classes, dataset_weights, dataset_configs, dataset_types
 
@@ -125,8 +114,8 @@ class OpenFoldDataModule(pl.LightningDataModule):
         Args:
             dataset_classes (list[Sequence[str]]): List of strings matching the specific OpenFoldSingleDataset classes to initialize.
             dataset_configs (list[Sequence[dict]]): List of configs to pass each dataset class.
-            dataset_types (list[Sequence[str]]): List of dataset types, elements can be train, valid, test, predict.
-            type_to_init (str): One of train, valid, test, predict.
+            dataset_types (list[Sequence[str]]): List of dataset types, elements can be train, validation, test, predict.
+            type_to_init (str): One of train, validation, test, predict.
 
         Returns:
             list[Sequence[OpenFoldSingleDataset]]: List of initialized OpenFoldSingleDataset objects.
@@ -138,51 +127,27 @@ class OpenFoldDataModule(pl.LightningDataModule):
             )
             if dataset_type == type_to_init
         ]
-        if (type_to_init in ["valid", "test", "predict"]) & (len(datasets) > 1):
+        if (type_to_init in ["validation", "test", "predict"]) & (len(datasets) > 1):
             warnings.warn(
                 f"{len(datasets)} {type_to_init} datasets were found, using only the first one."
             )
         return datasets
 
-    def generate_dataloader(self, stage: str):
-        """Wrap the appropriate dataset in a DataLoader and return it.
-
-        Args:
-            stage (str): Type of DataLoader to return, one of train, valid, test, predict.
-
-        Returns:
-            DataLoader: DataLoader object.
-        """
-        dataset = (
-            self.train_dataset
-            if stage == "train"
-            else self.valid_dataset
-            if stage == "valid"
-            else self.test_dataset
-            if stage == "test"
-            else self.predict_dataset
-        )
-
-        # TODO refactor OpenFoldDataLoader and add arguments
-        return DataLoader(
-            dataset=dataset,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            collate_fn=openfold_batch_collator,
-            generator=self.generator,
-        )
-
     def train_dataloader(self) -> DataLoader:
-        return self.generate_dataloader("train")
+        # TODO refactor OpneFoldDataLoader and add arguments
+        return DataLoader(self.train_dataset, "<other arguments>")
 
     def val_dataloader(self) -> DataLoader:
-        return self.generate_dataloader("valid")
+        # TODO refactor OpneFoldDataLoader and add arguments
+        return DataLoader(self.validation_dataset, "<other arguments>")
 
     def test_dataloader(self) -> DataLoader:
-        return self.generate_dataloader("test")
+        # TODO refactor OpneFoldDataLoader and add arguments
+        return DataLoader(self.test_dataset, "<other arguments>")
 
     def predict_dataloader(self) -> DataLoader:
-        return self.generate_dataloader("predict")
+        # TODO refactor OpneFoldDataLoader and add arguments
+        return DataLoader(self.predict_dataset, "<other arguments>")
 
 
 def openfold_batch_collator(prots):
---
+++ openfold3/core/data/data_structures/stochastic_sampler_dataset.py
@@ -0,0 +1,48 @@
+# TODO add license
+
+from typing import Any, Sequence
+
+import torch
+from torch.utils.data import Dataset
+
+
+class OpenFoldStochasticSamplerDataset(Dataset):
+    """A dataset class for combining multiple OpenFoldSingleDataset instances and sampling from them with the provided probabilities."""
+
+    def __init__(
+        self,
+        datasets: Sequence[Dataset],
+        probabilities: Sequence[float],
+        virtual_epoch_len: int,
+        num_virtual_epochs: int,
+        generator: torch.Generator,
+    ) -> None:
+        super().__init__()
+        if len(datasets) != len(probabilities):
+            raise RuntimeError(
+                "Number of datasets and probabilities must be equal."
+            )
+        self.datasets = datasets
+        self.probabilities = probabilities
+        self.virtual_epoch_len = virtual_epoch_len
+        self.num_virtual_epochs = num_virtual_epochs
+        self.generator = generator
+
+
+    def __len__(self):
+        return self.virtual_epoch_len
+
+    def __getitem__(self, index: tuple[int, int]) -> Any:
+        dataset_idx, datapoint_idx = self.indices[index]
+        return self.datasets[dataset_idx][datapoint_idx]
+
+    def resample_epoch(self):
+        """Resample virtual_epoch_len number of samples according to the provided probabilities."""
+        raise NotImplementedError(
+            "OpenFoldStochasticSamplerDataset.resample() is not yet implemented."
+        )
+        # <functions to generate index tuples self.indices for (dataset_idx, datapoint_idx)>
+        pass
+
+    def calculate_coverage(self):
+        """Calculate dataset coverage - low priority functionality."""
