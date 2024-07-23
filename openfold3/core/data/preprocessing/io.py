6f36307d8fbdfe34dcc4a04c436c0199c17987c9
Lukas Jarosch <lukas.jarosch@gmx.net>
2024-07-22 22:58:32 -0700

    Add extra_fields option and better docstring

--- openfold3/core/data/preprocessing/io.py
+++ openfold3/core/data/preprocessing/io.py
@@ -20,20 +20,25 @@ class ParsedStructure(NamedTuple):
 
 def parse_mmcif_bioassembly(
     file_path: Path | str,
-    use_author_fields=False,
-    include_bonds=True,
+    use_author_fields: bool = False,
+    include_bonds: bool = True,
+    extra_fields: list | None = None,
 ) -> ParsedStructure:
     """Convenience wrapper around biotite's bioassembly CIF parsing
 
-    Parses the mmCIF file, expands the first bioassembly, and creates an
-    AtomArray from it. This includes only the first model, resolves alternative
-    locations by taking the one with the highest occupancy, defaults to
-    inferring bond information, and defaults to using the PDB-automated
-    chain/residue annotation instead of author annotations.
+    Parses the mmCIF file, expands the first bioassembly, and creates an AtomArray from
+    it. This includes only the first model, resolves alternative locations by taking the
+    one with the highest occupancy, defaults to inferring bond information, and defaults
+    to using the PDB-automated chain/residue annotation instead of author annotations.
 
-    This function also creates a "chain_ids_renumbered" field in the AtomArray
-    with numerical chain IDs starting from 0, which can be useful when
-    bioassembly expansion results in multiple chains with the same chain ID.
+    This function also creates the following additional annotations in the AtomArray:
+        - occupancy: inferred from atom_site.occupancy
+        - entity_id: inferred from atom_site.label_entity_id
+        - atom_idx: numerical atom indices starting from 0
+        - molecule_type_id: numerical code for the molecule type (see tables.py)
+        - chain_id_renumbered: numerical chain IDs starting from 0 to circumvent
+          duplicate chain IDs after bioassembly expansion. This is used in place of the
+          original chain ID in all of the cleanup and preprocessing functions.
 
     Args:
         file_path:
@@ -42,6 +47,8 @@ def parse_mmcif_bioassembly(
             Whether to use author fields. Defaults to False.
         include_bonds:
             Whether to infer bond information. Defaults to True.
+        extra_fields:
+            Extra fields to include in the AtomArray. Defaults to None.
 
     Returns:
         A NamedTuple containing the parsed CIF file and the AtomArray.
@@ -50,6 +57,11 @@ def parse_mmcif_bioassembly(
 
     (pdb_id,) = cif_file.keys()  # Single-element unpacking
 
+    extra_fields_preset = ["label_entity_id", "occupancy"]
+
+    if extra_fields:
+        extra_fields_preset.extend(extra_fields)
+
     # Shared args between get_assembly and get_structure
     parser_args = {
         "pdbx_file": cif_file,
@@ -57,7 +69,7 @@ def parse_mmcif_bioassembly(
         "altloc": "occupancy",
         "use_author_fields": use_author_fields,
         "include_bonds": include_bonds,
-        "extra_fields": ["label_entity_id", "occupancy"],
+        "extra_fields": extra_fields_preset,
     }
 
     # Check if the CIF file contains bioassembly information
