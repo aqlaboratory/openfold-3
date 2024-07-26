e76076c915c407f08fd16d7ecb360078cbeb3726
Jennifer Wei <jennifer.wei@omsf.io>
2024-07-26 09:25:47 +0000

    Adds automatic import of runners and runner definitions to models

--- openfold3/model_implementations/__init__.py
+++ openfold3/model_implementations/__init__.py
@@ -1,1 +1,15 @@
-__all__ = ["af2_monomer", "af2_multimer", "af3_all_atom"]
+from pathlib import Path
+
+from openfold3.model_implementations import registry
+
+
+def _import_specific_py_files_from_dir(filename:str):
+    """Imports specific files upon initialization"""
+    for path in Path(__file__).parent.resolve().glob(f"*/{filename}"):
+        # Selects path starting from `openfold3` - will vary for different directories
+        package_module = '.'.join(list(path.parts[-4:-1]) + [str(path.stem)])
+        __import__(package_module)
+    return
+
+_ = _import_specific_py_files_from_dir("runner.py")
+MODEL_REGISTRY = registry.MODEL_REGISTRY
--- openfold3/model_implementations/registry.py
+++ openfold3/model_implementations/registry.py
@@ -1,59 +1,18 @@
 # TODO add license
-import dataclasses
+import functools
 import logging
-from ml_collections import ConfigDict
-from pathlib import Path
 from typing import Optional
 
 from openfold3.core.config import config_utils
-from openfold3.core.runners.model_runner import ModelRunner
+from openfold3.core.runners.registry_base import register_model_base
 
 # Record of ModelEntries
 MODEL_REGISTRY = {}
-
-
-@dataclasses.dataclass
-class ModelEntry:
-    name: str
-    model_factory: ModelRunner
-    base_config: ConfigDict
-    reference_config_path: Optional[Path]
-    # List of available presets, read from reference_config_path
-    presets: Optional[list[str]]
-
-
-def register_model(name, base_config, reference_config_path=None):
-    """Register a specific OpenFoldModelWrapper class in the MODEL_REGISTRY.
-
-    Args:
-        cls (Type[OpenFoldModelWrapper]): The class to register.
-
-    Returns:
-        Type[OpenFoldModelWrapper]: The registered class.
-    """
-
-    def _decorator(runner_cls):
-        if name in MODEL_REGISTRY:
-            raise ValueError(
-                "{name} has been previously registered in model_implementations.registry"
-            )
-        if reference_config_path:
-            reference_dict = config_utils.load_yaml(reference_config_path)
-            presets = list(reference_dict.keys())
-        else:
-            presets = None
-
-        MODEL_REGISTRY[name] = ModelEntry(
-            name, runner_cls, base_config, reference_config_path, presets
-        )
-        return runner_cls
-
-    # cls._registered = True  # QUESTION do we want to enforce class registration with
-    # this decorator? Part A.
-    return _decorator
+register_model = functools.partial(register_model_base, model_registry=MODEL_REGISTRY) 
 
 
 def make_config_with_preset(model_name: str, preset: Optional[str] = None):
+    """Retrieves config matching preset for one of the models."""
     base_config = MODEL_REGISTRY[model_name].base_config
     if preset is None:
         logging.info(f"Using default training configs for {model_name}")
@@ -74,12 +33,10 @@ def make_model_config(model_name: str, runner_yaml_path: str):
     Then updates the config the contents of the the runner_yaml itself.
     """
     runner_yaml_dict = config_utils.load_yaml(runner_yaml_path)
-    preset = (
-        runner_yaml_dict["model_preset"] if "model_preset" in runner_yaml_dict else None
-    )
-    preset_config = make_config_with_preset(model_name, preset)
+    preset = runner_yaml_dict.get("model_preset", None)
+    config = make_config_with_preset(model_name, preset)
     if "model_update" in runner_yaml_dict:
-        updated_config = config_utils.update_config_dict(
-            preset_config, runner_yaml_dict
+        config = config_utils.update_config_dict(
+            config, runner_yaml_dict["model_update"]
         )
-    return updated_config
+    return config
