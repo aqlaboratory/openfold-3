7abfa88d18ba9d5902a81968298b367860ddadfd
gnikolenyi <nikolenyi.gergo@gmail.com>
2024-07-29 17:24:30 -0400

    Modularize MSA pairing logic

--- openfold3/core/data/preprocessing/msa_primitives.py
+++ openfold3/core/data/preprocessing/msa_primitives.py
@@ -4,8 +4,32 @@ from typing import Sequence
 
 import numpy as np
 import pandas as pd
+from numpy.typing import NDArray
 
 from openfold3.core.data.preprocessing.io import Msa, MsaCollection, parse_msas_sample
+
+
+def extract_uniprot_hits(msa_collection: MsaCollection) -> dict[str, Msa]:
+    """Parses out UniProt Msa objects for unique protein chains from the MsaCollection.
+
+    Args:
+        msa_collection (MsaCollection):
+            A collection of Msa objects and chain IDs for a single sample.
+
+    Returns:
+        dict[str, Msa]:
+            Dict mapping chain IDs to Msa objects containing UniProt MSAs.
+    """
+    chain_ids = msa_collection.rep_msa_map.keys()
+    # Get uniprot hits
+    return {
+        chain_id: (
+            msa_collection.rep_msa_map[chain_id]["uniprot_hits"]
+            if "uniprot_hits" in msa_collection.rep_msa_map[chain_id]
+            else msa_collection.rep_msa_map[chain_id]["uniprot"]
+        )
+        for chain_id in chain_ids
+    }
 
 
 def process_uniprot_metadata(msa: Msa) -> None:
@@ -62,94 +86,132 @@ def calculate_distance_to_query(msa: Msa) -> None:
     ] / float(sum(msa_array[0, :] != "-"))
 
 
-def find_pairing_indices(series_list) -> None:
-    # Combine into single dataframe
-    max_len = max(len(s) for s in series_list)
-    df = pd.DataFrame({i: pd.Series(s) for i, s in enumerate(series_list)}).fillna('NaN')
-
-    # Stack the DataFrame to count occurrences
-    stacked = df.stack()
-    counts = stacked.value_counts()
-
-    # Determine the maximum count of occurrences
-    max_occurrence = max(counts)
-
-    # Prepare a dictionary to hold indices for each count
-    occurrence_dict = {i: [] for i in range(1, max_occurrence + 1)}
-
-    # Populate the dictionary with elements and their counts
-    for elem, count in counts.items():
-        if elem == 'NaN':
-            continue
-        indices = [(s_idx, series_list[s_idx][series_list[s_idx] == elem].index.tolist()) for s_idx in range(len(series_list)) if elem in series_list[s_idx].values]
-        occurrence_dict[count].extend(indices)
-
-    sorted_indices = []
-    for count in range(max_occurrence, 0, -1):
-        elements = occurrence_dict[count]
-        sorted_indices.extend(elements)
-
-
-def find_shared_species(uniprot_hits: dict[str, Msa],
-                        paired_row_cutoff: int) -> None:
-    """Finds species shared between at least two unique protein chains.
-
-    Adds an is_shared_species column to the header DataFrame of an Msa object,
-    indicating if the row corresponds a sequence from a species that occurs in
-    at least two UNIQUE chains.
+def count_species_per_chain(
+    uniprot_hits: dict[str, Msa],
+) -> tuple[NDArray[np.int32], list[str]]:
+    """Counts the occurrences of sequences from species in each chain's UniProt MSA.
 
     Args:
-        uniprot_hits (dict[str, Msa]): 
-            Dictionary mapping representative chain IDs to parsed UniProt Msa objects.
+        uniprot_hits (dict[str, Msa]):
+            Dict mapping chain IDs to Msa objects containing UniProt MSAs.
+
+    Returns:
+        NDArray[np.int32], list[str]:
+            The array of occurrence counts (number of chains x number of unique species)
+            and the list species with at least one sequence among MSAs of all chains.
     """
-
-    chain_ids = uniprot_hits.keys()
-    n_unique_chains = len(chain_ids)
-
-    # Process uniprot headers and calculate distance to query
-    _ = [process_uniprot_metadata(uniprot_hits[chain_id]) for chain_id in chain_ids]
-    _ = [calculate_distance_to_query(uniprot_hits[chain_id]) for chain_id in chain_ids]
-
     species = []
-    _ = [species.extend(set(uniprot_hits[chain_id].metadata["species_id"])) for chain_id in chain_ids]
+    _ = [
+        species.extend(set(uniprot_hits[chain_id].metadata["species_id"]))
+        for chain_id in uniprot_hits
+    ]
     species = list(set(species))
     species_index = np.arange(len(species))
     species_index_map = {species[i]: i for i in species_index}
 
     # Get lists of species per chain
-    species_index_per_chain = [np.array(uniprot_hits[chain_id].metadata["species_id"].apply(lambda x: species_index_map[x])) for chain_id in chain_ids]
+    species_index_per_chain = [
+        np.array(
+            uniprot_hits[chain_id]
+            .metadata["species_id"]
+            .apply(lambda x: species_index_map[x])
+        )
+        for chain_id in uniprot_hits
+    ]
 
     # Combine all lists into one array
     all_lists = np.concatenate(species_index_per_chain)
 
     # List to keep track of which list each element came from
-    list_indices = np.concatenate([np.array([i]*len(lst)) for i, lst in enumerate(species_index_per_chain)])
+    list_indices = np.concatenate(
+        [np.array([i] * len(lst)) for i, lst in enumerate(species_index_per_chain)]
+    )
 
     # Get unique integers and their inverse mapping to reconstruct original array
     unique_integers, inverse_indices = np.unique(all_lists, return_inverse=True)
 
     # Initialize the 2D array to count occurrences
-    count_array = np.zeros((n_unique_chains, len(unique_integers)), dtype=int)
+    count_array = np.zeros((len(uniprot_hits), len(unique_integers)), dtype=int)
 
     # Use np.add.at for unbuffered in-place addition
     np.add.at(count_array, (list_indices, inverse_indices), 1)
 
-    # Find species that are shared by at least two chains
-    is_shared_species = np.sum(count_array != 0, axis=0) > 1
+    return count_array, species
 
-    # Find species that occur more than 600 times in any single chain
-    is_nonprevalent_species = np.sum(count_array <= 600, axis=0) == n_unique_chains
 
+def get_pairing_masks(
+    count_array: NDArray[np.int32], mask_keys: Sequence[str]
+) -> NDArray[np.bool_]:
+    """Generates masks for the pairing process.
+
+    Useful for excluding things like species that occur only in on chain (will not be
+    pairable) or species that occur too frequently in the MSA of a single chain (as done
+    in the AF2-Multimer pairing code).
+
+    Args:
+        count_array (NDArray[np.int32]):
+            The array of species occurrence counts per chain.
+        mask_keys (Sequence[str]):
+            List of strings indicating which mask to add.
+
+    Returns:
+        NDArray[np.bool_]: The union of all masks to apply during pairing.
+    """
+    pairing_masks = np.ones(count_array.shape[1], dtype=bool)
+
+    if "shared_by_two" in mask_keys:
+        # Find species that are shared by at least two chains
+        pairing_masks = pairing_masks & np.sum(count_array != 0, axis=0) > 1
+
+    if "less_than_600" in mask_keys:
+        # Find species that occur more than 600 times in any single chain
+        pairing_masks = (
+            pairing_masks & np.sum(count_array <= 600, axis=0) == count_array.shape[0]
+        )
+
+    return pairing_masks
+
+
+def find_pairing_indices(
+    count_array: NDArray[np.int32],
+    pairing_masks: NDArray[np.bool_],
+    paired_row_cutoff: int,
+) -> tuple[np.ndarray, np.ndarray]:
+    """The main function for finding indices that pair rows in the MSA arrays.
+
+    This function follows the AF2-Multimer strategy for pairing rows of UniProt MSAs but
+    excludes block-diagonal elements (unpaired rows).
+
+    Args:
+        count_array (NDArray[np.int32]):
+            The array of species occurrence counts per chain
+        pairing_masks (NDArray[np.bool_]):
+            The union of all masks to apply during pairing.
+        paired_row_cutoff (int):
+            The maximum number of rows to pair.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]:
+            A tuple of arrays containing the indices that pair rows in MSAs of an
+            assembly across chains and the mask for partially paired rows for each chain
+            that cannot be fully paired.
+    """
     # Apply filters
-    count_array_filtered = count_array[:, is_shared_species & is_nonprevalent_species]
-    species_index_filtered = species_index[is_shared_species & is_nonprevalent_species]
+    species_index = np.arange(count_array.shape[-1])
+    count_array_filtered = count_array[:, pairing_masks]
+    species_index_filtered = species_index[pairing_masks]
     is_in_chain_per_species = count_array != 0
 
     # Iterative row-subtraction
+    n_unique_chains = count_array.shape[0]
     paired_species_rows = []  # species indices in the MSA feature format
     missing_species_rows = []  # mask for missing species per chain
     n_rows = 0  # number of paired rows
     for n in np.arange(1, n_unique_chains + 1)[::-1]:
+        # Break if n is 1 since we are excluding block-diagonal elements
+        if n == 1:
+            break
+
         # Find which species are shared by exactly n chains
         is_in_n_chains = sum(count_array_filtered != 0) == n
 
@@ -157,62 +219,78 @@ def find_shared_species(uniprot_hits: dict[str, Msa],
         min_in_n_chains = np.min(count_array_filtered[:, is_in_n_chains], axis=0)
 
         # Expand filtered species index across occurrences and chains
-        cols = np.tile(np.repeat(species_index_filtered, min_in_n_chains), (n_unique_chains, 1))
+        cols = np.tile(
+            np.repeat(species_index_filtered, min_in_n_chains), (n_unique_chains, 1)
+        )
         n_rows += cols.shape[1]
         paired_species_rows.append(cols.T)
 
-        # Create mask for elements where species is missing form chain
+        # Create mask for elements where species is missing from chain
         missing_species_mask = is_in_chain_per_species[:, cols[0, :]]
         missing_species_rows.append(missing_species_mask.T)
 
         # Subtract min per row
         count_array_filtered -= min_in_n_chains
 
-        # Break if row cutoff reached or n is 1 since we are excluding block-diagonal elements
-        if (n_rows >= paired_row_cutoff) | (n == 1):
+        # If row cutoff reached, crop final arrays to the row cutoff and break
+        if n_rows >= paired_row_cutoff:
+            n_rows_final = paired_row_cutoff - sum(
+                [rows.shape[0] for rows in paired_species_rows[:-1]]
+            )
+            paired_species_rows[-1] = paired_species_rows[-1][: n_rows_final + 1, :]
+            missing_species_mask[-1] = missing_species_mask[-1][: n_rows_final + 1, :]
             break
 
-    # Used paired_species_rows indices to return rows from MSA arrays
-    # and mask using missing_species_rows
+        # Concatenate all paired arrays into a single paired array
+        paired_rows_index = np.concatenate(paired_species_rows, axis=0)
+        missing_rows_index = np.concatenate(missing_species_rows, axis=0)
 
-    # Return an array of paired rows of string arrays
-    # Q: does the gap due to missing species need to be different from gap due to alignment?
+    return paired_rows_index, missing_rows_index
 
 
-def create_paired(msa_collection: MsaCollection) -> None:
+def create_paired(
+    msa_collection: MsaCollection, paired_row_cutoff: int
+) -> NDArray[np.str_]:
     """Creates paired MSA arrays from UniProt MSAs.
 
-        Follows the AF2-Multimer strategy for pairing rows of UniProt MSAs based on
-        species IDs, but excludes the block-diagonal rows.
+    Follows the AF2-Multimer strategy for pairing rows of UniProt MSAs based on species
+    IDs, but excludes the block-diagonal rows, i.e. only includes rows which can be at
+    least partially paired.
 
     Args:
         msa_collection (MsaCollection):
             A collection of Msa objects and chain IDs for a single sample.
+        paired_row_cutoff (int):
+            The maximum number of rows to pair.
     """
-    chain_ids = msa_collection.rep_msa_map.keys()
-    # Get uniprot hits
-    uniprot_hits = {
-        chain_id: (
-            msa_collection.rep_msa_map[chain_id]["uniprot_hits"]
-            if "uniprot_hits" in msa_collection.rep_msa_map[chain_id]
-            else msa_collection.rep_msa_map[chain_id]["uniprot"]
-        )
-        for chain_id in chain_ids
-    }
+    # Get parsed uniprot hits
+    uniprot_hits = extract_uniprot_hits(msa_collection)
+
     # Process uniprot headers and calculate distance to query
-    _ = [process_uniprot_metadata(uniprot_hits[chain_id]) for chain_id in chain_ids]
-    _ = [calculate_distance_to_query(uniprot_hits[chain_id]) for chain_id in chain_ids]
+    _ = [process_uniprot_metadata(uniprot_hits[chain_id]) for chain_id in uniprot_hits]
+    _ = [
+        calculate_distance_to_query(uniprot_hits[chain_id]) for chain_id in uniprot_hits
+    ]
 
-    # Find shared species
-    find_shared_species(uniprot_hits)
+    # Count species occurences per chain
+    count_array, species = count_species_per_chain(uniprot_hits)
 
-    # TODO CONTINUE FROM HERE
-    """
-    How to go from metadata of distances to query and shared species to
-    pairing
-    sorting
-    concatenating
-    """
+    # Get pairing masks
+    pairing_masks = get_pairing_masks(count_array, ["shared_by_two", "less_than_600"])
+
+    # Find indices that pair rows as outlined in the AF3 SI of the AF2-Multimer strategy
+    paired_rows_index, missing_rows_index = find_pairing_indices(
+        count_array,
+        pairing_masks,
+        paired_row_cutoff,
+    )
+
+    # Sort by similarity to query
+
+    # Map indices to string arrays with gaps
+
+    # Return string array
+
     return
 
 
