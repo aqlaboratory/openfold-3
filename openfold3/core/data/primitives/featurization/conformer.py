d34dc9433a33c9b5dd6603d9a3f5d93b52fbd60a
gnikolenyi <nikolenyi.gergo@gmail.com>
2024-08-04 20:46:13 -0400

    Publish featurization branch

--- openfold3/core/data/primitives/featurization/conformer.py
+++ openfold3/core/data/primitives/featurization/conformer.py
@@ -1,1 +1,4 @@
 """This module contains building blocks for conformer feature generation."""
+
+# Contains reference conformer features:
+# https://docs.google.com/document/d/1ASyORivaElmgpiwce7wi8GURlvAG5wxtCNgitRZTpEU/edit#heading=h.camfkytl072o
--- openfold3/core/data/primitives/featurization/custom.py
+++ openfold3/core/data/primitives/featurization/custom.py
@@ -1,1 +1,4 @@
 """This module contains building blocks for custom feature generation."""
+
+# Contains custom feature generators:
+# https://docs.google.com/document/d/1ASyORivaElmgpiwce7wi8GURlvAG5wxtCNgitRZTpEU/edit#heading=h.tpwihmbzuwh6
