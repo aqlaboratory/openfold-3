9b9e4f8d1ef178b06821ff36015a5a2de8f3c32c
gnikolenyi <nikolenyi.gergo@gmail.com>
2024-08-08 12:45:21 -0400

    Format code.

--- openfold3/core/data/primitives/featurization/msa.py
+++ openfold3/core/data/primitives/featurization/msa.py
@@ -1,4 +1,1 @@
 """This module contains building blocks for MSA feature generation."""
-
-# Contains MSA feature generators:
-# https://docs.google.com/document/d/1ASyORivaElmgpiwce7wi8GURlvAG5wxtCNgitRZTpEU/edit#heading=h.h7v693xi378p
