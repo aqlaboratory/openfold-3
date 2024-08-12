Change cdist back to manual computation for bf16, quick fix for indexing issues with bf16 for atomization utils

openfold3/core/loss/confidence.py
@@ -209,19 +209,19 @@ def express_coords_in_frames(
     a, b, c = phi
     w1 = a - b
     w2 = c - b
-    w1_norm = torch.linalg.norm(w1, dim=-1, keepdim=True)
-    w2_norm = torch.linalg.norm(w2, dim=-1, keepdim=True)
-    w1 = w1 / (w1_norm + eps)
-    w2 = w2 / (w2_norm + eps)
+    w1_norm = torch.sqrt(eps**2 + torch.sum(w1**2, dim=-1, keepdim=True))
+    w2_norm = torch.sqrt(eps**2 + torch.sum(w2**2, dim=-1, keepdim=True))
+    w1 = w1 / w1_norm
+    w2 = w2 / w2_norm
 
     # Build orthonormal basis
     # [*, N_token, 3]
     e1 = w1 + w2
     e2 = w2 - w1
-    e1_norm = torch.linalg.norm(e1, dim=-1, keepdim=True)
-    e2_norm = torch.linalg.norm(e2, dim=-1, keepdim=True)
-    e1 = e1 / (e1_norm + eps)
-    e2 = e2 / (e2_norm + eps)
+    e1_norm = torch.sqrt(eps**2 + torch.sum(e1**2, dim=-1, keepdim=True))
+    e2_norm = torch.sqrt(eps**2 + torch.sum(e2**2, dim=-1, keepdim=True))
+    e1 = e1 / e1_norm
+    e2 = e2 / e2_norm
 
     # BF16-friendly cross product
     e3 = [
@@ -276,7 +276,7 @@ def compute_alignment_error(
     """
     xij = express_coords_in_frames(x=x, phi=phi, eps=eps)
     xij_gt = express_coords_in_frames(x=x_gt, phi=phi_gt, eps=eps)
-    return torch.dist(xij, xij_gt)
+    return torch.sqrt(eps**2 + torch.sum((xij - xij_gt) ** 2, dim=-1))
 
 
 def all_atom_plddt_loss(
@@ -312,8 +312,12 @@ def all_atom_plddt_loss(
     # Compute difference in distances
     # [*, N_atom, N_atom]
     x_gt = batch["ground_truth"]["atom_positions"]
-    dx = torch.cdist(x, x)
-    dx_gt = torch.cdist(x_gt, x_gt)
+    dx = torch.sqrt(
+        eps**2 + torch.sum((x[..., None, :] - x[..., None, :, :]) ** 2, dim=-1)
+    )
+    dx_gt = torch.sqrt(
+        eps**2 + torch.sum((x_gt[..., None, :] - x_gt[..., None, :, :]) ** 2, dim=-1)
+    )
     d = torch.abs(dx_gt - dx)
 
     # Compute pair mask based on distance and type of atom m
@@ -342,7 +346,7 @@ def all_atom_plddt_loss(
         1 - batch["is_atomized"]
     )
 
-    restype = batch["restype"].to(dtype=x.dtype)
+    restype = batch["restype"]
     ca_atom_index_offset, ca_atom_mask = get_token_atom_index_offset(
         atom_name="CA", restype=restype
     )
@@ -546,8 +550,13 @@ def pde_loss(
     )
 
     # Compute prediction target
-    d = torch.cdist(rep_x, rep_x)
-    d_gt = torch.cdist(rep_x_gt, rep_x_gt)
+    d = torch.sqrt(
+        eps**2 + torch.sum((rep_x[..., None, :] - rep_x[..., None, :, :]) ** 2, dim=-1)
+    )
+    d_gt = torch.sqrt(
+        eps**2
+        + torch.sum((rep_x_gt[..., None, :] - rep_x_gt[..., None, :, :]) ** 2, dim=-1)
+    )
     e = torch.abs(d - d_gt)
 
     # Compute binned prediction target

openfold3/core/loss/diffusion.py
@@ -173,8 +173,12 @@ def bond_loss(batch: Dict, x: torch.Tensor, eps: float) -> torch.Tensor:
     atom_mask_gt = batch["ground_truth"]["atom_resolved_mask"]
 
     # Compute pairwise distances
-    dx = torch.cdist(x, x)
-    dx_gt = torch.cdist(x_gt, x_gt)
+    dx = torch.sqrt(
+        eps**2 + torch.sum((x[..., None, :] - x[..., None, :, :]) ** 2, dim=-1)
+    )
+    dx_gt = torch.sqrt(
+        eps**2 + torch.sum((x_gt[..., None, :] - x_gt[..., None, :, :]) ** 2, dim=-1)
+    )
 
     # Construct polymer-ligand per-token bond mask
     # TODO: double check this
@@ -228,8 +232,12 @@ def smooth_lddt_loss(batch: Dict, x: torch.Tensor, eps: float) -> torch.Tensor:
     atom_mask_gt = batch["ground_truth"]["atom_resolved_mask"]
 
     # [*, N_atom, N_atom]
-    dx = torch.cdist(x, x)
-    dx_gt = torch.cdist(x_gt, x_gt)
+    dx = torch.sqrt(
+        eps**2 + torch.sum((x[..., None, :] - x[..., None, :, :]) ** 2, dim=-1)
+    )
+    dx_gt = torch.sqrt(
+        eps**2 + torch.sum((x_gt[..., None, :] - x_gt[..., None, :, :]) ** 2, dim=-1),
+    )
 
     # [*, N_atom, N_atom]
     d = torch.abs(dx_gt - dx)

openfold3/core/utils/atomize_utils.py
@@ -42,7 +42,7 @@ def broadcast_token_feat_to_atoms(
     feat_dims = token_feat.shape[token_dim:][1:]
 
     # Apply token mask
-    num_atoms_per_token = num_atoms_per_token * token_mask
+    num_atoms_per_token = num_atoms_per_token * token_mask.int()
     token_feat = token_feat * token_mask.reshape(
         (*batch_dims, n_token, *((1,) * len(feat_dims)))
     )
@@ -56,11 +56,9 @@ def broadcast_token_feat_to_atoms(
             [token_feat, torch.zeros_like(token_feat)], dim=token_dim
         ).reshape((*batch_dims, 2 * n_token, *feat_dims))
 
-    # TODO: Check why when running torch.max(torch.sum(num_atoms_per_token, dim=-1))
-    #  in bf16, the sum is wrong
     # Pad token features
     # Flatten batch and token dimensions
-    max_num_atoms = torch.max(torch.sum(num_atoms_per_token.float(), dim=-1)).int()
+    max_num_atoms = torch.max(torch.sum(num_atoms_per_token, dim=-1)).int()
     padded_token_feat = torch.concat(
         [
             token_feat,
@@ -250,22 +248,20 @@ def get_token_atom_index_offset(atom_name: str, restype: torch.Tensor):
     """
     token_atom_index_offset = torch.einsum(
         "...k,k->...",
-        restype,
+        restype.float(),
         torch.tensor(
             atom_name_to_index_by_restype[atom_name]["index"],
             device=restype.device,
-            dtype=restype.dtype,
-        ),
-    )
+        ).float(),
+    ).long()
     token_atom_mask = torch.einsum(
         "...k,k->...",
-        restype,
+        restype.float(),
         torch.tensor(
             atom_name_to_index_by_restype[atom_name]["mask"],
             device=restype.device,
-            dtype=restype.dtype,
-        ),
-    )
+        ).float(),
+    ).long()
     return token_atom_index_offset, token_atom_mask
 
 
@@ -298,7 +294,7 @@ def get_token_center_atoms(batch: Dict, x: torch.Tensor, atom_mask: torch.Tensor
         1 - batch["is_atomized"]
     )
 
-    restype = batch["restype"].to(dtype=x.dtype)
+    restype = batch["restype"]
     protein_token_atom_index_offset, protein_token_atom_mask = (
         get_token_atom_index_offset(atom_name="CA", restype=restype)
     )
@@ -385,7 +381,7 @@ def get_token_representative_atoms(
     )
 
     # Get index of representative atoms
-    restype = batch["restype"].to(dtype=x.dtype)
+    restype = batch["restype"]
     start_atom_index = batch["start_atom_index"].long()
     cb_atom_index_offset, cb_atom_mask = get_token_atom_index_offset(
         atom_name="CB", restype=restype
@@ -504,7 +500,7 @@ def get_token_frame_atoms(
         1 - batch["is_atomized"]
     )
 
-    restype = batch["restype"].to(dtype=x.dtype)
+    restype = batch["restype"]
     n_atom_index_offset, n_atom_mask = get_token_atom_index_offset(
         atom_name="N", restype=restype
     )

openfold3/model_implementations/af3_all_atom/model.py
@@ -358,7 +358,7 @@ class AlphaFold3(nn.Module):
 
         return output
 
-    def forward(self, batch: Dict) -> Dict:
+    def forward(self, batch: Dict) -> [Dict, Dict]:
         """
         Args:
             batch:
@@ -463,7 +463,8 @@ class AlphaFold3(nn.Module):
                         *"atom_resolved_mask" ([*, N_atom])
                             Mask for ground truth atom positions
         Returns:
-            Output dictionary containing the following keys:
+            batch: Updated batch dictionary post permutation alignment
+            output: Output dictionary containing the following keys:
                 "si_trunk" ([*, N_token, C_s]):
                     Single representation output from model trunk
                 "zij_trunk" ([*, N_token, N_token, C_z]):
@@ -534,4 +535,4 @@ class AlphaFold3(nn.Module):
 
                 output.update(diffusion_output)
 
-        return output
+        return batch, output

tests/data_utils.py
@@ -190,79 +190,59 @@ def random_af3_features(batch_size, n_token, n_msa, n_templ):
         "residue_index": torch.arange(0, n_token)
         .unsqueeze(0)
         .repeat((batch_size, 1))
-        .to(dtype=torch.int32),
+        .int(),
         "token_index": torch.arange(0, n_token)
         .unsqueeze(0)
         .repeat((batch_size, 1))
-        .to(dtype=torch.int32),
-        "asym_id": asym_id.to(dtype=torch.int32),
-        "entity_id": asym_id.clone().to(dtype=torch.int32),
-        "sym_id": torch.ones((batch_size, n_token)).to(dtype=torch.int32),
-        "restype": restypes_one_hot.unsqueeze(0)
-        .repeat((batch_size, 1, 1))
-        .to(dtype=torch.int32),
-        "is_protein": is_protein.unsqueeze(0)
-        .repeat((batch_size, 1))
-        .to(dtype=torch.int32),
-        "is_dna": is_dna.unsqueeze(0).repeat((batch_size, 1)).to(dtype=torch.int32),
-        "is_rna": is_rna.unsqueeze(0).repeat((batch_size, 1)).to(dtype=torch.int32),
-        "is_ligand": torch.zeros((batch_size, n_token)).to(dtype=torch.int32),
-        "is_atomized": torch.zeros((batch_size, n_token)).to(dtype=torch.int32),
+        .int(),
+        "asym_id": asym_id.int(),
+        "entity_id": asym_id.clone().int(),
+        "sym_id": torch.ones((batch_size, n_token)).int(),
+        "restype": restypes_one_hot.unsqueeze(0).repeat((batch_size, 1, 1)).int(),
+        "is_protein": is_protein.unsqueeze(0).repeat((batch_size, 1)).int(),
+        "is_dna": is_dna.unsqueeze(0).repeat((batch_size, 1)).int(),
+        "is_rna": is_rna.unsqueeze(0).repeat((batch_size, 1)).int(),
+        "is_ligand": torch.zeros((batch_size, n_token)).int(),
+        "is_atomized": torch.zeros((batch_size, n_token)).int(),
         # Reference conformation features
-        "ref_pos": torch.randn((batch_size, n_atom, 3)).to(dtype=torch.float32),
-        "ref_mask": torch.ones((batch_size, n_atom)).to(dtype=torch.int32),
-        "ref_element": torch.ones((batch_size, n_atom, 128)).to(dtype=torch.int32),
-        "ref_charge": torch.ones((batch_size, n_atom)).to(dtype=torch.float32),
-        "ref_atom_name_chars": torch.ones((batch_size, n_atom, 4, 64)).to(
-            dtype=torch.int32
-        ),
-        "ref_space_uid": torch.zeros((batch_size, n_atom)).to(dtype=torch.int32),
+        "ref_pos": torch.randn((batch_size, n_atom, 3)).float(),
+        "ref_mask": torch.ones((batch_size, n_atom)).int(),
+        "ref_element": torch.ones((batch_size, n_atom, 128)).int(),
+        "ref_charge": torch.ones((batch_size, n_atom)).float(),
+        "ref_atom_name_chars": torch.ones((batch_size, n_atom, 4, 64)).int(),
+        "ref_space_uid": torch.zeros((batch_size, n_atom)).int(),
         # MSA features
-        "msa": torch.ones((batch_size, n_msa, n_token, 32)).to(dtype=torch.int32),
-        "has_deletion": torch.ones((batch_size, n_msa, n_token)).to(
-            dtype=torch.float32
-        ),
-        "deletion_value": torch.ones((batch_size, n_msa, n_token)).to(
-            dtype=torch.float32
-        ),
-        "profile": torch.ones((batch_size, n_token, 32)).to(dtype=torch.float32),
-        "deletion_mean": torch.ones((batch_size, n_token)).to(dtype=torch.float32),
+        "msa": torch.ones((batch_size, n_msa, n_token, 32)).int(),
+        "has_deletion": torch.ones((batch_size, n_msa, n_token)).float(),
+        "deletion_value": torch.ones((batch_size, n_msa, n_token)).float(),
+        "profile": torch.ones((batch_size, n_token, 32)).float(),
+        "deletion_mean": torch.ones((batch_size, n_token)).float(),
         # Template features
-        "template_restype": torch.ones((batch_size, n_templ, n_token, 32)).to(
-            dtype=torch.int32
-        ),
-        "template_pseudo_beta_mask": torch.ones((batch_size, n_templ, n_token)).to(
-            dtype=torch.float32
-        ),
-        "template_backbone_frame_mask": torch.ones((batch_size, n_templ, n_token)).to(
-            dtype=torch.float32
-        ),
+        "template_restype": torch.ones((batch_size, n_templ, n_token, 32)).int(),
+        "template_pseudo_beta_mask": torch.ones((batch_size, n_templ, n_token)).float(),
+        "template_backbone_frame_mask": torch.ones(
+            (batch_size, n_templ, n_token)
+        ).float(),
         "template_distogram": torch.ones(
             (batch_size, n_templ, n_token, n_token, 39)
-        ).to(dtype=torch.int32),
+        ).int(),
         "template_unit_vector": torch.ones(
             (batch_size, n_templ, n_token, n_token, 3)
-        ).to(dtype=torch.float32),
+        ).float(),
         # Bond features
-        "token_bonds": torch.ones((batch_size, n_token, n_token)).to(dtype=torch.int32),
+        "token_bonds": torch.ones((batch_size, n_token, n_token)).int(),
         # Additional features
-        "token_mask": torch.ones((batch_size, n_token)).to(dtype=torch.float32),
-        "start_atom_index": start_atom_index.unsqueeze(0)
-        .repeat((batch_size, 1))
-        .to(dtype=torch.int32),
+        "token_mask": torch.ones((batch_size, n_token)).float(),
+        "start_atom_index": start_atom_index.unsqueeze(0).repeat((batch_size, 1)).int(),
         "num_atoms_per_token": num_atoms_per_token.unsqueeze(0)
         .repeat((batch_size, 1))
-        .to(dtype=torch.int32),
-        "msa_mask": torch.ones((batch_size, n_msa, n_token)).to(dtype=torch.float32),
-        "num_paired_seqs": torch.Tensor([int(n_msa / 2)]).to(dtype=torch.int32),
-        "resolution": torch.Tensor([2.0]).to(dtype=torch.float32),
+        .int(),
+        "msa_mask": torch.ones((batch_size, n_msa, n_token)).float(),
+        "num_paired_seqs": torch.Tensor([int(n_msa / 2)]).int(),
+        "resolution": torch.Tensor([2.0]).float(),
         "is_distillation": torch.BoolTensor([False]),
         "ground_truth": {
-            "atom_positions": torch.randn((batch_size, n_atom, 3)).to(
-                dtype=torch.float32
-            ),
-            "atom_resolved_mask": torch.ones((batch_size, n_atom)).to(
-                dtype=torch.float32
-            ),
+            "atom_positions": torch.randn((batch_size, n_atom, 3)).float(),
+            "atom_resolved_mask": torch.ones((batch_size, n_atom)).float(),
         },
     }

tests/test_af3_model.py
@@ -25,7 +25,7 @@ class TestAF3Model(unittest.TestCase):
     ):
         device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        config = registry.MODEL_REGISTRY["af3_all_atom"].base_config
+        config = registry.make_config_with_preset("af3_all_atom")
 
         if train:
             config.globals.chunk_size = None
@@ -70,12 +70,10 @@ class TestAF3Model(unittest.TestCase):
 
         batch = tensor_tree_map(to_device, batch)
 
-        torch.cuda.empty_cache()
-
         if train:
             af3_loss = AlphaFold3Loss(config=config.loss)
 
-            outputs = af3(batch=batch)
+            batch, outputs = af3(batch=batch)
 
             loss, loss_breakdown = af3_loss(
                 batch=batch, output=outputs, _return_breakdown=True
@@ -96,7 +94,7 @@ class TestAF3Model(unittest.TestCase):
             af3.eval()
 
             with torch.no_grad():
-                outputs = af3(batch=batch)
+                _, outputs = af3(batch=batch)
 
             atom_positions_predicted = outputs["atom_positions_predicted"]
 
@@ -169,12 +167,15 @@ class TestAF3Model(unittest.TestCase):
                 use_block_sparse=True,
             )
 
+    @unittest.skip(
+        "Manually enable this for now, will add flag to run slow tests later."
+    )
     @compare_utils.skip_unless_triton_installed()
     @compare_utils.skip_unless_cuda_available()
     def test_shape_large_eval(self):
         batch_size = 1
         n_token = 384
-        n_msa = 16384
+        n_msa = 8000
         n_templ = 4
 
         for dtype in [torch.float32, torch.bfloat16]:
@@ -190,12 +191,15 @@ class TestAF3Model(unittest.TestCase):
                 use_block_sparse=True,
             )
 
+    @unittest.skip(
+        "Manually enable this for now, will add flag to run slow tests later."
+    )
     @compare_utils.skip_unless_triton_installed()
     @compare_utils.skip_unless_cuda_available()
     def test_shape_large_bf16_train(self):
         batch_size = 1
         n_token = 384
-        n_msa = 16384
+        n_msa = 8000
         n_templ = 4
 
         self.run_model(