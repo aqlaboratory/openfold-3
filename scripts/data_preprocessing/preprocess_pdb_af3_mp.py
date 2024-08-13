0d164cc6cc8eafc111695795467625be28c61654
Lukas Jarosch <lukas.jarosch@gmx.net>
2024-08-13 15:17:22 -0700

    Add multi-processing version of the PDB script

---
+++ scripts/data_preprocessing/preprocess_pdb_af3_mp.py
@@ -0,0 +1,450 @@
+import json
+import logging
+import os
+from collections import defaultdict
+from multiprocessing import Manager
+from multiprocessing.pool import Pool
+from pathlib import Path
+from typing import NamedTuple
+
+import biotite.structure as struc
+import click
+import numpy as np
+from biotite.structure import AtomArray
+from biotite.structure.io.pdbx import CIFFile
+from pdbeccdutils.core import ccd_reader
+from rdkit import Chem
+from tqdm import tqdm
+
+from openfold3.core.data.io import utils
+from openfold3.core.data.io.sequence.fasta import write_annotated_chains_fasta
+from openfold3.core.data.io.structure.cif import parse_mmcif, write_minimal_cif
+from openfold3.core.data.pipelines.preprocessing.structure import (
+    cleanup_structure_af3,
+)
+from openfold3.core.data.primitives.structure.cleanup import remove_terminal_atoms
+from openfold3.core.data.primitives.structure.interface import (
+    get_interface_chain_id_pairs,
+)
+from openfold3.core.data.primitives.structure.labels import (
+    get_chain_to_author_chain_dict,
+    get_chain_to_entity_dict,
+    get_chain_to_molecule_type_dict,
+    get_chain_to_pdb_chain_dict,
+)
+from openfold3.core.data.primitives.structure.ligand import (
+    mol_from_atomarray,
+    mol_from_parsed_component,
+)
+from openfold3.core.data.primitives.structure.metadata import (
+    get_chain_to_canonical_seq_dict,
+    get_cif_block,
+    get_pdb_id,
+    get_release_date,
+    get_resolution,
+)
+from openfold3.core.data.primitives.structure.unresolved import add_unresolved_atoms
+from openfold3.core.data.resources.tables import MoleculeType
+
+print("PID:", os.getpid())
+
+ChainID = str
+EntityID = int
+ChemCompID = str
+
+
+class AssemblyTooLargeError(Exception):
+    """Raised when the assembly is too large to process."""
+
+
+# TODO: move explanation to docstring
+class ProcessedStructure(NamedTuple):
+    cif_file: CIFFile  # parsed CIF file information
+    atom_array: AtomArray  # cleaned structure
+    metadata_dict: dict  # structure-, chain-, and interface-level metadata
+    chain_to_canonical_seq: dict[
+        ChainID, str
+    ]  # mapping of chain IDs to canonical sequences
+    ligand_ccd_components_to_chains: dict[ChemCompID, list[ChainID]]
+    other_ccd_components: list[
+        ChemCompID
+    ]  # 3-letter codes of residue names that are in the CCD
+    special_ligand_entities: list[EntityID]
+
+
+def process_structure(cif_path: Path, ccd: CIFFile) -> ProcessedStructure:
+    cif_file, atom_array = parse_mmcif(
+        cif_path, expand_bioassembly=True, extra_fields=["auth_asym_id"]
+    )
+
+    if struc.get_chain_count(atom_array) > 300:
+        raise AssemblyTooLargeError
+
+    # Get block with all the primary CIF data
+    cif_data = get_cif_block(cif_file)
+
+    # Apply full structure cleanup
+    atom_array = cleanup_structure_af3(atom_array, cif_data, ccd)
+
+    # Infer interface pairs
+    interface_chain_pairs = get_interface_chain_id_pairs(atom_array)
+
+    # Add unresolved atoms explicitly with NaN coordinates
+    atom_array = add_unresolved_atoms(atom_array, cif_data, ccd)
+
+    # Remove terminal atoms
+    atom_array = remove_terminal_atoms(atom_array)
+
+    # Get basic metadata
+    release_date = get_release_date(cif_data).strftime("%Y-%m-%d")
+    metadata_dict = {
+        "release_date": release_date,
+        "resolution": get_resolution(cif_data),
+    }
+
+    # Get chain-level metadata
+    chain_to_pdb_chain = get_chain_to_pdb_chain_dict(atom_array)
+    chain_to_author_chain = get_chain_to_author_chain_dict(atom_array)
+    chain_to_entity = get_chain_to_entity_dict(atom_array)
+    chain_to_molecule_type = get_chain_to_molecule_type_dict(atom_array)
+    chain_to_canonical_seq = get_chain_to_canonical_seq_dict(atom_array, cif_data)
+
+    # Create list of chains with metadata
+    chain_metadata_list = []
+
+    for chain in chain_to_pdb_chain:
+        chain_metadata = {
+            "molecule_type": chain_to_molecule_type[chain],
+            "chain_id": chain,
+            "chain_id_pdb": chain_to_pdb_chain[chain],
+            "chain_id_author": chain_to_author_chain[chain],
+            "entity_id": chain_to_entity[chain],
+        }
+        chain_metadata_list.append(chain_metadata)
+
+    # Create list of interfaces (TODO: split into primitive)
+    interface_metadata_list = []
+
+    for chain_1, chain_2 in interface_chain_pairs:
+        interface_metadata = {
+            "chain_id": [chain_1, chain_2],
+        }
+        interface_metadata_list.append(interface_metadata)
+
+    # Add metadata lists to the metadata dictionary
+    metadata_dict["chains"] = chain_metadata_list
+    metadata_dict["interfaces"] = interface_metadata_list
+
+    ligand_ccd_components_to_chains, other_ccd_components, special_ligand_entities = (
+        get_components(atom_array)
+    )
+
+    return ProcessedStructure(
+        cif_file=cif_file,
+        atom_array=atom_array,
+        metadata_dict=metadata_dict,
+        chain_to_canonical_seq=chain_to_canonical_seq,
+        ligand_ccd_components_to_chains=ligand_ccd_components_to_chains,
+        other_ccd_components=other_ccd_components,
+        special_ligand_entities=special_ligand_entities,
+    )
+
+
+class ProcessedComponents(NamedTuple):
+    ligand_ccd_components_to_chains: dict[ChemCompID, list[ChainID]]
+    other_ccd_components: list[ChemCompID]
+    special_ligand_entities: list[EntityID]
+
+
+def get_components(atom_array: AtomArray) -> ProcessedComponents:
+    ligand_ccd_components_to_chains = defaultdict(list)
+    special_ligand_entities = set()
+
+    # Get ligand components
+    ligand_filter = atom_array.molecule_type_id == MoleculeType.LIGAND
+    ligand_atom_array = atom_array[ligand_filter]
+
+    if ligand_atom_array.array_length() != 0:
+        for ligand_chain in struc.chain_iter(ligand_atom_array):
+            # Append standard single-residue ligands to ligand CCD components
+            if struc.get_residue_count(ligand_chain) == 1:
+                ccd_id = ligand_chain.res_name[0]
+                chain_id = ligand_chain.chain_id[0]
+                ligand_ccd_components_to_chains[ccd_id].append(chain_id)
+            # Append special ligands that do not correspond to a single CCD ID to
+            # special ligand entities
+            else:
+                special_ligand_entities.add(ligand_chain.entity_id[0])
+
+    # Get other non-ligand standard components like protein and nucleic acid residues
+    other_ccd_components = set()
+    for resname in np.unique(atom_array[~ligand_filter].res_name):
+        other_ccd_components.add(resname)
+
+    return ProcessedComponents(
+        ligand_ccd_components_to_chains=dict(ligand_ccd_components_to_chains),
+        other_ccd_components=list(other_ccd_components),
+        special_ligand_entities=list(special_ligand_entities),
+    )
+
+
+class ManagerSet:
+    """A set-like object that can be shared across processes using a Manager."""
+
+    def __init__(self, manager):
+        self.dict = manager.dict()
+
+    def add(self, item):
+        self.dict[item] = None
+
+    def __contains__(self, item):
+        return item in self.dict
+
+    def __len__(self):
+        return len(self.dict)
+
+
+@click.command()
+@click.option(
+    "--mmcif_folder",
+    type=click.Path(file_okay=False, dir_okay=True, exists=True, path_type=Path),
+    help="Folder containing mmCIF files",
+)
+@click.option(
+    "--ccd_path",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True, path_type=Path),
+    help="Path to a Chemical Component Dictionary (CCD) .cif file",
+)
+@click.option(
+    "--components_cif_folder",
+    type=click.Path(file_okay=False, dir_okay=True, exists=True, path_type=Path),
+    help="Path to a folder containing .cif files for CCD components",
+)
+@click.option(
+    "--output_folder",
+    type=click.Path(file_okay=False, dir_okay=True, exists=False, path_type=Path),
+    help="Folder to save cleaned bCIF files and other extracted files in",
+)
+@click.option(
+    "--include_cifs",
+    type=bool,
+    default=False,
+    help="Whether to save additional .cif files alongside the .bcif files",
+)
+@click.option(
+    "--num_processes",
+    type=int,
+    default=None,
+    help="Number of processes to use for multiprocessing",
+)
+def main(
+    mmcif_folder: Path,
+    ccd_path: Path,
+    components_cif_folder: Path,
+    output_folder: Path,
+    include_cifs: bool,
+    num_processes: int | None,
+) -> None:
+    setup_folders(output_folder)
+
+    # Biotite-parsed CCD for regular metadata access
+    ccd = CIFFile.read(ccd_path)
+
+    # # Preprocessed Component objects for every CCD component returned by pdbeccdutils
+    # logger.info("Parsing CCD components...")
+    # parsed_ccd_components = {
+    #     ccd_id: result.component
+    #     for ccd_id, result in ccd_reader.read_pdb_components_file(str(ccd_path)).items()
+    # }
+    logger.info("Starting processing...")
+
+    logger.debug("Getting list of CIF files...")
+    cif_files = list(mmcif_folder.glob("*.cif"))[:1_000]
+    logger.debug("Creating Manager...")
+    manager = Manager()
+    # logger.debug("Creating parsed_ccd_components...")
+    # logger.debug(f"{type(parsed_ccd_components)}")
+    # parsed_ccd_components = manager.dict(parsed_ccd_components)
+    logger.debug("Creating all_metadata...")
+    all_metadata = manager.dict()  # Using Manager to handle metadata across processes
+    logger.debug("Creating ccd_component_to_canonical_smiles...")
+    ccd_component_to_canonical_smiles = manager.dict()
+    logger.debug("Creating saved_ccd_components...")
+    saved_ccd_components = ManagerSet(manager)
+
+    logger.debug("Starting multi-processing...")
+    with Pool(num_processes) as pool:
+        list(
+            tqdm(
+                pool.imap(
+                    process_file,
+                    [
+                        (
+                            path,
+                            ccd,
+                            components_cif_folder,
+                            output_folder,
+                            include_cifs,
+                            all_metadata,
+                            ccd_component_to_canonical_smiles,
+                            saved_ccd_components,
+                        )
+                        for path in cif_files
+                    ],
+                    chunksize=10,
+                ),
+                total=len(cif_files),
+            )
+        )
+
+    # Write the collected metadata to JSON
+    with open(output_folder / "metadata.json", "w") as f:
+        json.dump(
+            dict(all_metadata), f, indent=4, default=utils.encode_numpy_types
+        )  # Convert Manager dict to regular dict
+
+
+def setup_folders(output_folder: Path):
+    output_folder.mkdir(parents=True, exist_ok=True)
+    (output_folder / "cif_files").mkdir(exist_ok=True)
+    (output_folder / "ccd_component_sdfs").mkdir(exist_ok=True)
+
+
+def process_file(args):
+    (
+        cif_path,
+        ccd,
+        components_cif_folder,
+        output_folder,
+        include_cifs,
+        all_metadata,
+        ccd_component_to_canonical_smiles,
+        saved_ccd_components,
+    ) = args
+
+    try:
+        try:
+            processed_structure = process_structure(cif_path, ccd)
+        except AssemblyTooLargeError:
+            logger.info(f"Skipping structure {cif_path} due to too large assembly")
+            return
+
+        pdb_id = get_pdb_id(processed_structure.cif_file)
+        structure_metadata = processed_structure.metadata_dict
+        atom_array = processed_structure.atom_array
+
+        output_cif_folder = output_folder / "cif_files"
+        ccd_components_path = output_folder / "ccd_component_sdfs"
+
+        # Make output subfolders
+        output_cif_subfolder = output_cif_folder / pdb_id
+        output_cif_subfolder.mkdir(exist_ok=True, parents=True)
+        special_component_sdfs_path = output_cif_subfolder / "special_ligand_sdfs"
+        special_component_sdfs_path.mkdir(exist_ok=True)
+
+        ccd_id_to_chains = processed_structure.ligand_ccd_components_to_chains
+
+        for ccd_id, chains in ccd_id_to_chains.items():
+            if ccd_id in saved_ccd_components:
+                canonical_smiles = ccd_component_to_canonical_smiles[ccd_id]
+            else:
+                # TODO: get_component() function
+                ccd_result = ccd_reader.read_pdb_cif_file(
+                    str(components_cif_folder / f"{ccd_id}.cif")
+                )
+                component = ccd_result.component
+                mol = mol_from_parsed_component(
+                    component, assign_fallback_conformer=True
+                )
+                canonical_smiles = Chem.MolToSmiles(mol)
+                ccd_component_to_canonical_smiles[ccd_id] = canonical_smiles
+
+                with Chem.SDWriter(ccd_components_path / f"{ccd_id}.sdf") as writer:
+                    writer.write(mol)
+
+                saved_ccd_components.add(ccd_id)
+
+            for chain_metadata in structure_metadata["chains"]:
+                if chain_metadata["chain_id"] in chains:
+                    chain_metadata["canonical_smiles"] = canonical_smiles
+
+        for ccd_id in processed_structure.other_ccd_components:
+            if ccd_id in saved_ccd_components:
+                continue
+
+            ccd_result = ccd_reader.read_pdb_cif_file(
+                str(components_cif_folder / f"{ccd_id}.cif")
+            )
+            component = ccd_result.component
+            mol = mol_from_parsed_component(component, assign_fallback_conformer=True)
+
+            with Chem.SDWriter(ccd_components_path / f"{ccd_id}.sdf") as writer:
+                writer.write(mol)
+
+            saved_ccd_components.add(ccd_id)
+
+        for entity_id in processed_structure.special_ligand_entities:
+            entity_atom_array = atom_array[atom_array.entity_id == entity_id]
+
+            # Get first chain of entity
+            first_entity_chain_id = entity_atom_array.chain_id[0]
+            first_entity_chain_atom_array = entity_atom_array[
+                entity_atom_array.chain_id == first_entity_chain_id
+            ]
+
+            mol = mol_from_atomarray(
+                first_entity_chain_atom_array, assign_fallback_conformer=True
+            )
+
+            mol_sdf_path = special_component_sdfs_path / f"entity_{entity_id}.sdf"
+            with Chem.SDWriter(mol_sdf_path) as writer:
+                writer.write(mol)
+
+            for chain_metadata in structure_metadata["chains"]:
+                if chain_metadata["entity_id"] == entity_id:
+                    chain_metadata["canonical_smiles"] = Chem.MolToSmiles(mol)
+
+        # Set final metadata for structure
+        all_metadata[pdb_id] = structure_metadata
+
+        # Save cleaned bCIF file (this will be minimal and only include AtomSite
+        # & bond records)
+        bcif_path = output_cif_subfolder / f"{pdb_id}.bcif"
+        write_minimal_cif(
+            processed_structure.atom_array,
+            bcif_path,
+            format="bcif",
+            include_bonds=True,
+        )
+
+        # Save additional minimal .cif file if requested
+        if include_cifs:
+            cif_path = output_cif_subfolder / f"{pdb_id}.cif"
+            write_minimal_cif(
+                processed_structure.atom_array,
+                cif_path,
+                format="cif",
+                include_bonds=True,
+            )
+
+        # Save FASTA file
+        write_annotated_chains_fasta(
+            output_cif_subfolder / f"{pdb_id}.fasta",
+            processed_structure.chain_to_canonical_seq,
+            pdb_id,
+            processed_structure.metadata_dict["chains"],
+        )
+
+    except Exception:
+        logger.exception(f"Error processing {cif_path}")
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger("openfold3")
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    main()
