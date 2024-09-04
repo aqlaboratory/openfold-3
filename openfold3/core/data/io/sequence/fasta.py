0c16402ebdf9b4071450d0505649ae8cc7e2b095
Lukas Jarosch <lukas.jarosch@gmx.net>
2024-09-04 15:09:08 -0700

    Improve variable names and type hint

--- openfold3/core/data/io/sequence/fasta.py
+++ openfold3/core/data/io/sequence/fasta.py
@@ -77,26 +77,24 @@ def consolidate_preprocessed_fastas(preprocessed_dir: Path) -> dict[str, str]:
 
 def write_multichain_fasta(
     output_path: Path,
-    chain_to_sequence: dict,
+    id_to_sequence: dict[str, str],
 ) -> Path:
-    """Writes a FASTA file from a dictionary of chain IDs to sequences.
+    """Writes a FASTA file from a dictionary of IDs to sequences.
 
     The output FASTA will follow the format:
-    >{chain_id}
+    >{id}
     {sequence}
 
     Args:
         output_path:
             Path to write the FASTA file to.
         chain_to_sequence:
-            Dictionary mapping chain IDs to sequences.
+            Dictionary mapping IDs to sequences.
 
     Returns:
         Path to the written FASTA file.
     """
     with open(output_path, "w") as file:
-        file.writelines(
-            f">{chain_id}\n{seq}\n" for chain_id, seq in chain_to_sequence.items()
-        )
+        file.writelines(f">{id_}\n{seq}\n" for id_, seq in id_to_sequence.items())
 
     return output_path
