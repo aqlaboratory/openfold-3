Added batching changes for msa module embedder, all atom confidence heads, and loss weighting. Added DS kernel option for pairformer embedding in aux heads. Fixed previous compilation graph breaks: removed use of .item() and added compiler ignore header to DS kernel function.

openfold3/core/loss/diffusion.py
@@ -21,6 +21,7 @@ from typing import Callable, Dict, Optional
 import torch
 from torch.utils.checkpoint import checkpoint
 
+from openfold3.core.loss.loss_utils import loss_masked_batch_mean
 from openfold3.core.utils.atomize_utils import broadcast_token_feat_to_atoms
 from openfold3.core.utils.tensor_utils import tensor_tree_map
 
@@ -345,9 +346,12 @@ def diffusion_loss(
         mean_loss:
             Diffusion loss
         loss_breakdown:
-            Dict of individual component losses
+            Dict of individual component losses (unweighted)
     """
     loss_weights = batch["loss_weights"]
+    mse_weight = loss_weights["mse"]
+    bond_weight = loss_weights["bond"]
+    smooth_lddt_weight = loss_weights["smooth_lddt"]
 
     # Expand to broadcast to number of samples
     batch = tensor_tree_map(lambda t: t.unsqueeze(1), batch)
@@ -360,53 +364,64 @@ def diffusion_loss(
         ligand_weight=ligand_weight,
         eps=eps,
     )
-    loss_breakdown = {"mse_loss": l_mse}
+    loss_breakdown = {"mse": l_mse.mean(dim=-1)}
 
     l_bond = 0.0
     l_smooth_lddt = 0.0
-    bond_weight = loss_weights["bond"]
-    smooth_lddt_weight = loss_weights["smooth_lddt"]
     if chunk_size is None:
-        if bond_weight > 0:
+        if bond_weight.any():
             l_bond = bond_loss(x=x, batch=batch, eps=eps)
-            loss_breakdown["bond_loss"] = l_bond
+            loss_breakdown["bond"] = l_bond.mean(dim=-1)
 
-        if smooth_lddt_weight > 0:
+        if smooth_lddt_weight.any():
             l_smooth_lddt = smooth_lddt_loss(x=x, batch=batch, eps=eps)
-            loss_breakdown["smooth_lddt_loss"] = l_smooth_lddt
+            loss_breakdown["smooth_lddt"] = l_smooth_lddt.mean(dim=-1)
     else:
-        if bond_weight > 0:
+        if bond_weight.any():
             l_bond = run_low_mem_loss_fn(
                 loss_fn=bond_loss,
                 x=x,
                 kwargs={"batch": batch, "eps": eps},
                 chunk_size=chunk_size,
             )
-            loss_breakdown["bond_loss"] = l_bond
+            loss_breakdown["bond"] = l_bond.mean(dim=-1)
 
-        if smooth_lddt_weight > 0:
+        if smooth_lddt_weight.any():
             l_smooth_lddt = run_low_mem_loss_fn(
                 loss_fn=smooth_lddt_loss,
                 x=x,
                 kwargs={"batch": batch, "eps": eps},
                 chunk_size=chunk_size,
             )
-            loss_breakdown["smooth_lddt_loss"] = l_smooth_lddt
+            loss_breakdown["smooth_lddt"] = l_smooth_lddt.mean(dim=-1)
 
+    # Mean over batch dimension for individual losses
+    # Mask out samples where the loss is disabled
     loss_breakdown = {
-        k: torch.mean(v).detach().clone() for k, v in loss_breakdown.items()
+        f"{name}_loss": loss_masked_batch_mean(
+            loss=loss, weight=loss_weights[name], apply_weight=False, eps=eps
+        )
+        .detach()
+        .clone()
+        for name, loss in loss_breakdown.items()
     }
 
-    l_mse = l_mse * loss_weights["mse"]
-    l_bond = l_bond * bond_weight
-    l_smooth_lddt = l_smooth_lddt * smooth_lddt_weight
+    l_mse = l_mse * mse_weight.unsqueeze(-1)
+    l_bond = l_bond * bond_weight.unsqueeze(-1)
+    l_smooth_lddt = l_smooth_lddt * smooth_lddt_weight.unsqueeze(-1)
 
-    # TODO: Changed from SI, denominator (t + sigma_data) ** 2 changed
-    #  to (t * sigma_data) ** 2. Test whether this is works as expected.
+    # Note: Changed from SI, denominator (t + sigma_data) ** 2 changed
+    #  to (t * sigma_data) ** 2.
     w = (t**2 + sigma_data**2) / (t * sigma_data) ** 2
     l = w * (l_mse + l_bond) + l_smooth_lddt
 
-    mean_loss = torch.mean(l)
+    # Mean over diffusion sample dimension
+    mean_loss = torch.mean(l, dim=-1)
+
+    # Mean over batch dimension, only for samples with diffusion losses enabled
+    mean_loss = loss_masked_batch_mean(
+        loss=mean_loss, weight=mse_weight, apply_weight=False, eps=eps
+    )
 
     # Return to original batch size
     tensor_tree_map(lambda t: t.squeeze(1), batch)

openfold3/core/loss/loss_module.py
@@ -168,7 +168,7 @@ class AlphaFold3Loss(nn.Module):
 
         cum_loss = 0.0
         losses = {}
-        if confidence_weights_sum > 0:
+        if confidence_weights_sum.any():
             l_confidence, l_confidence_breakdown = confidence_loss(
                 batch=batch, output=output, **self.config.confidence
             )
@@ -179,13 +179,15 @@ class AlphaFold3Loss(nn.Module):
             # Weighted in confidence_loss()
             cum_loss = cum_loss + l_confidence
 
+        # Run diffusion loss only if diffusion training and losses are enabled
+        atom_positions_diffusion = output.get("atom_positions_diffusion")
         diffusion_weights_sum = sum(
             loss_weights[k].float() for k in self.config.diffusion_loss_names
         )
-        if diffusion_weights_sum > 0:
+        if atom_positions_diffusion is not None and diffusion_weights_sum.any():
             l_diffusion, l_diffusion_breakdown = diffusion_loss(
                 batch=batch,
-                x=output["atom_positions_diffusion"],
+                x=atom_positions_diffusion,
                 t=output["noise_level"],
                 **self.config.diffusion,
             )
@@ -196,13 +198,16 @@ class AlphaFold3Loss(nn.Module):
             # Weighted in diffusion_loss()
             cum_loss = cum_loss + l_diffusion
 
-        if loss_weights["distogram"] > 0:
-            l_distogram = all_atom_distogram_loss(
+        if loss_weights["distogram"].any():
+            l_distogram, l_distogram_breakdown = all_atom_distogram_loss(
                 batch=batch, logits=output["distogram_logits"], **self.config.distogram
             )
-            losses["distogram_loss"] = l_distogram.detach().clone()
+            losses.update(l_distogram_breakdown)
 
-            cum_loss = cum_loss + loss_weights["distogram"].item() * l_distogram
+            losses["scaled_distogram_loss"] = l_distogram.detach().clone()
+
+            # Weighted in all_atom_distogram_loss()
+            cum_loss = cum_loss + l_distogram
 
         losses["loss"] = cum_loss.detach().clone()
 

openfold3/core/model/feature_embedders/input_embedders.py
@@ -576,6 +576,58 @@ class MSAModuleEmbedder(nn.Module):
             c_s_input, c_m, **linear_init_params.linear_s_input
         )
 
+    @staticmethod
+    def subsample_msa(
+        msa_feat: torch.Tensor,
+        msa_mask: torch.Tensor,
+        num_main_msa_seqs: torch.Tensor,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Subsample main MSA features for a single element in the batch.
+
+        Args:
+            msa_feat:
+                [N_seq, N_token, c_m_feats] MSA features
+            msa_mask:
+                [N_seq, N_token] MSA mask
+            num_main_msa_seqs:
+                Number of main MSA sequences to sample from
+        Returns:
+            sampled_msa:
+                [N_seq_sampled, N_token, c_m_feats] Sampled MSA features
+            msa_mask:
+                [N_seq_sampled, N_token] Sampled MSA mask
+        """
+        total_msa_seq = msa_feat.shape[-3]
+
+        # Split uniprot and main MSA sequences. Only main MSA seqs will be sampled.
+        # All uniprot seqs are in the final MSA representation.
+        num_paired_seqs = num_main_msa_seqs.int()
+
+        if num_main_msa_seqs.any():
+            num_main_msa_seqs = total_msa_seq - num_paired_seqs
+            split_sections = [num_paired_seqs, num_main_msa_seqs]
+            uniprot_msa, main_msa = torch.split(msa_feat, split_sections, dim=-3)
+            uniprot_msa_mask, main_msa_mask = torch.split(
+                msa_mask, split_sections, dim=-2
+            )
+
+            # Sample Uniform[1, num_main_msa_seqs] sequences from the main MSA
+            n_seq_sample = torch.randint(low=1, high=num_main_msa_seqs + 1, size=(1,))
+            index_order = torch.randperm(num_main_msa_seqs, device=msa_feat.device)
+            index_order = index_order[:n_seq_sample]
+
+            main_msa = torch.index_select(main_msa, dim=-3, index=index_order)
+            main_msa_mask = torch.index_select(main_msa_mask, dim=-2, index=index_order)
+
+            # Combine uniprot and sampled main MSA sequences
+            sampled_msa = torch.cat([uniprot_msa, main_msa], dim=-3)
+            msa_mask = torch.cat([uniprot_msa_mask, main_msa_mask], dim=-2)
+
+        else:
+            sampled_msa = msa_feat
+
+        return sampled_msa, msa_mask
+
     def forward(
         self, batch: Dict, s_input: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -597,6 +649,7 @@ class MSAModuleEmbedder(nn.Module):
             msa_mask:
                 [*, N_seq, N_token] MSA mask
         """
+        batch_dims = batch["msa"].shape[:-3]
         msa_feat = torch.cat(
             [
                 batch["msa"],
@@ -607,35 +660,39 @@ class MSAModuleEmbedder(nn.Module):
         )
         msa_mask = batch["msa_mask"]
 
-        total_msa_seq = batch["msa"].shape[-3]
+        # Unbind batch dim if it exists, and subsample msa seqs per batch
+        if len(batch_dims) > 0:
+            per_batch_msa = torch.unbind(msa_feat, dim=0)
+            per_batch_mask = torch.unbind(msa_mask, dim=0)
+            num_paired_seqs = torch.unbind(batch["num_paired_seqs"], dim=0)
 
-        # Split uniprot and main MSA sequences. Only main MSA seqs will be sampled.
-        # All uniprot seqs are in the final MSA representation.
-        num_paired_seqs = int(batch["num_paired_seqs"].item())
-        num_main_msa_seqs = total_msa_seq - num_paired_seqs
+            per_batch_sampled_msa = [
+                self.subsample_msa(m, mask, n)
+                for m, mask, n in zip(per_batch_msa, per_batch_mask, num_paired_seqs)
+            ]
 
-        if num_main_msa_seqs > 0:
-            split_sections = [num_paired_seqs, num_main_msa_seqs]
-            uniprot_msa, main_msa = torch.split(msa_feat, split_sections, dim=-3)
-            uniprot_msa_mask, main_msa_mask = torch.split(
-                msa_mask, split_sections, dim=-2
+            sampled_msa = [m[0] for m in per_batch_sampled_msa]
+            msa_mask = [m[1] for m in per_batch_sampled_msa]
+            max_msa_seqs = max([m.shape[-3] for m in sampled_msa])
+
+            def pad_batch_msas(m: torch.Tensor, seq_dim: int) -> torch.Tensor:
+                # Pad MSA sequence dimension to max seqs in batch
+                non_pad_dims = (0,) * 2 * (abs(seq_dim) - 1)
+                return torch.nn.functional.pad(
+                    m, (*non_pad_dims, 0, max_msa_seqs - m.shape[seq_dim])
+                )
+
+            sampled_msa = torch.stack(
+                [pad_batch_msas(m, seq_dim=-3) for m in sampled_msa], dim=0
+            )
+            msa_mask = torch.stack(
+                [pad_batch_msas(m, seq_dim=-2) for m in msa_mask], dim=0
             )
 
-            # Sample Uniform[1, num_main_msa_seqs] sequences from the main MSA
-            n_seq_sample = torch.randint(
-                low=1, high=num_main_msa_seqs + 1, size=(1,)
-            ).item()
-            index_order = torch.randperm(num_main_msa_seqs, device=msa_feat.device)
-            index_order = index_order[:n_seq_sample]
-
-            main_msa = torch.index_select(main_msa, dim=-3, index=index_order)
-            main_msa_mask = torch.index_select(main_msa_mask, dim=-2, index=index_order)
-
-            # Combine uniprot and sampled main MSA sequences
-            sampled_msa = torch.cat([uniprot_msa, main_msa], dim=-3)
-            msa_mask = torch.cat([uniprot_msa_mask, main_msa_mask], dim=-2)
         else:
-            sampled_msa = msa_feat
+            sampled_msa, msa_mask = self.subsample_msa(
+                msa_feat, msa_mask, batch["num_paired_seqs"]
+            )
 
         m = self.linear_m(sampled_msa)
         m = m + self.linear_s_input(s_input).unsqueeze(-3)

openfold3/core/model/heads/head_modules.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict
+from typing import Dict, Optional
 
 import torch
 import torch.nn as nn
@@ -162,7 +162,11 @@ class AuxiliaryHeadsAllAtom(nn.Module):
         batch: Dict,
         si_input: torch.Tensor,
         output: Dict,
-        chunk_size: int,
+        chunk_size: Optional[int] = None,
+        use_deepspeed_evo_attention: bool = False,
+        use_lma: bool = False,
+        inplace_safe: bool = False,
+        _mask_trans: bool = True,
     ):
         """
         Args:
@@ -178,7 +182,18 @@ class AuxiliaryHeadsAllAtom(nn.Module):
                         Pair representation output from model trunk
                     "atom_positions_predicted" ([*, N_atom, 3]):
                         Predicted atom positions
-            chunk_size: Feat associated with pairformer stack (int)
+            chunk_size:
+                Inference-time subbatch size. Associated with PairFormer embedding.
+            use_deepspeed_evo_attention:
+                Whether to use DeepSpeed memory efficient kernel.
+                Mutually exclusive with use_lma.
+            use_lma:
+                Whether to use low-memory attention during inference.
+                Mutually exclusive with use_deepspeed_evo_attention.
+            inplace_safe:
+                Whether inplace operations can be performed
+            _mask_trans:
+                Whether to mask the output of the transition layers
 
         Returns:
             aux_out:
@@ -239,6 +254,10 @@ class AuxiliaryHeadsAllAtom(nn.Module):
             single_mask=repr_x_mask,
             pair_mask=pair_mask,
             chunk_size=chunk_size,
+            use_deepspeed_evo_attention=use_deepspeed_evo_attention,
+            use_lma=use_lma,
+            inplace_safe=inplace_safe,
+            _mask_trans=_mask_trans,
         )
 
         # Get atom mask padded to MAX_ATOMS_PER_TOKEN

openfold3/core/model/primitives/attention.py
@@ -699,7 +699,7 @@ class GlobalAttention(nn.Module):
         return m
 
 
-@torch.jit.ignore
+@torch.compiler.disable
 def _deepspeed_evo_attn(
     q: torch.Tensor,
     k: torch.Tensor,

openfold3/projects/af3_all_atom/model.py
@@ -239,6 +239,7 @@ class AlphaFold3(nn.Module):
         si_input: torch.Tensor,
         si_trunk: torch.Tensor,
         zij_trunk: torch.Tensor,
+        inplace_safe: bool = False,
     ) -> Dict:
         """
         Mini diffusion rollout described in section 4.1.
@@ -253,6 +254,8 @@ class AlphaFold3(nn.Module):
                 [*, N_token, C_s] Single representation output from model trunk
             zij_trunk:
                 [*, N_token, N_token, C_z] Pair representation output from model trunk
+            inplace_safe:
+                Whether inplace operations can be performed
 
         Returns:
             Output dictionary containing the predicted trunk embeddings,
@@ -297,6 +300,10 @@ class AlphaFold3(nn.Module):
                 si_input=si_input,
                 output=output,
                 chunk_size=self.settings.chunk_size,
+                use_deepspeed_evo_attention=self.settings.use_deepspeed_evo_attention,
+                use_lma=self.settings.use_lma,
+                inplace_safe=inplace_safe,
+                _mask_trans=True,
             )
         )
 
@@ -539,7 +546,11 @@ class AlphaFold3(nn.Module):
 
         # Mini rollout
         output = self._rollout(
-            batch=batch, si_input=si_input, si_trunk=si_trunk, zij_trunk=zij_trunk
+            batch=batch,
+            si_input=si_input,
+            si_trunk=si_trunk,
+            zij_trunk=zij_trunk,
+            inplace_safe=inplace_safe,
         )
 
         if self.training:  # noqa: SIM102