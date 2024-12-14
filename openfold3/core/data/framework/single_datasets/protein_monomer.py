146e6d789fb69892a6c15fac7891ce121d43a3be
Vinay Swamy <swamyvinny@gmail.com>
2024-12-13 16:26:17 -0800

    refactor functionality of WeightedPDBClass into a base class, that others can inherit from

--- openfold3/core/data/framework/single_datasets/protein_monomer.py
+++ openfold3/core/data/framework/single_datasets/protein_monomer.py
@@ -1,22 +1,25 @@
-#%%
-import json
-import pandas as pd 
-from openfold3.core.data.framework.single_datasets.abstract_single_dataset import register_dataset
-from openfold3.core.data.framework.single_datasets.pdb import WeightedPDBDataset
+# %%
+import pandas as pd
+
+from openfold3.core.data.framework.single_datasets.abstract_single_dataset import (
+    register_dataset,
+)
+from openfold3.core.data.framework.single_datasets.base_single_dataset import (
+    BaseSingleDataset
+)
+
 
 @register_dataset
-class MonomerDistillationDataset(WeightedPDBDataset):
+class MonomerDistillationDataset(BaseSingleDataset):
     def create_datapoint_cache(self):
         """
         Same premise as the ValidationDatasetClass - no need for chain/interface
         distinctions or sample weighting - just need to iterate over all entries
         - "datapoint" is used here -> preferred_chain_or_interface = datapoint["datapoint"]
-        setting this to None uses just the chain 
+        setting this to None uses just the chain
         """
-        pdb_ids = []
-        for entry, _ in self.dataset_cache.structure_data.items():
-            pdb_ids.append((entry, None, -1))
-
         self.datapoint_cache = pd.DataFrame(
-            pdb_ids, columns=["pdb_id", "datapoint", "weight"]
-        )
+            list(self.dataset_cache.structure_data.keys()), columns=["pdb_id"]
+        ).assign(weight=1.0) 
+        ## weight column requried by StochasticSampler. Setting this to 1 
+        ## weights all samples equally(weight is passed to torch.multinomial)
