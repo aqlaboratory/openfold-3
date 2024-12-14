9515e3b352f53d7cae8d5e6e98f5fcf25d6a73a1
Vinay Swamy <swamyvinny@gmail.com>
2024-12-13 16:27:48 -0800

    add docstring to function

--- openfold3/core/data/io/utils.py
+++ openfold3/core/data/io/utils.py
@@ -1,20 +1,37 @@
-import numpy as np
 import boto3
+import numpy as np
 
 
-def download_file_from_s3(bucket: str, prefix: str, filename: str, outfile: str, profile: str| None = None, session: boto3.Session | None = None):
+def download_file_from_s3(
+    bucket: str,
+    prefix: str,
+    filename: str,
+    outfile: str,
+    profile: str | None = None,
+    session: boto3.Session | None = None,
+):
+    """ Download a file from an s3 bucket, using the provided profile or session.
+    Args:
+        bucket (str): the name of the s3 bucket. Must not have s3:// prefix
+        prefix (str): the path from the bucket root to the dir containing the file
+        filename (str): name of the file to download
+        outfile (str): file to save the downloaded file to
+        profile (str | None, optional): profile to instantiate the boto3 session with
+        session (boto3.Session | None, optional): instantiated boto3 session to use. 
+        one of profile or session must be provided
+        
+    """
     if session is None:
         if profile is None:
             raise ValueError("Either profile or session must be provided")
-        session = boto3.Session(profile_name=profile) 
+        session = boto3.Session(profile_name=profile)
     s3_client = session.client("s3")
     try:
         s3_client.download_file(bucket, f"{prefix}/{filename}", outfile)
     except Exception as e:
         print(f"Error downloading file from s3://{bucket}/{prefix}/{filename}")
         raise e
-    return 
-
+    return
 
 
 def encode_numpy_types(obj: object):
