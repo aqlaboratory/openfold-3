Bug fixes for confidence heads: Added layer norms and fixed loss weighting. Additional bug fix for BF16 sum in atomize utils.

openfold3/core/model/layers/attention_pair_bias.py
@@ -235,13 +235,13 @@ class AttentionPairBias(nn.Module):
         n_key = 128
         n_atom = a.shape[-2]
 
-        offset = n_query // 2 - 0.5
-        n_center = int(n_atom // n_query) + 1
-        subset_centers = offset + torch.arange(n_center, device=a.device) * n_query
+        offset = n_query // 2
+
+        num_blocks = math.ceil(n_atom / n_query)
+        subset_centers = offset + torch.arange(num_blocks) * n_query
 
         pad_len_right_q = (n_query - n_atom % n_query) % n_query
         a_query = torch.nn.functional.pad(a, (0, 0, 0, pad_len_right_q), value=0.0)
-        num_blocks = a_query.shape[-2] // n_query
 
         a_query = convert_to_blocks_1d(
             x=a_query,
@@ -251,8 +251,8 @@ class AttentionPairBias(nn.Module):
             num_blocks=num_blocks,
         )
 
-        pad_len_right_k = math.ceil(subset_centers[-1]) + n_key // 2 - n_atom
-        pad_len_left_k = n_key // 2 - math.ceil(subset_centers[0])
+        pad_len_right_k = subset_centers[-1] + n_key // 2 - n_atom
+        pad_len_left_k = n_key // 2 - subset_centers[0]
         a_key = torch.nn.functional.pad(
             a, (0, 0, pad_len_left_k, pad_len_right_k), value=0.0
         )

openfold3/core/utils/atomize_utils.py
@@ -135,9 +135,10 @@ def aggregate_atom_feat_to_tokens(
     # Mask out atoms that are not part of the structure
     # Padding value must be greater than the largest index so that it
     # is properly excluded from the aggregation
-    atom_to_token_index = atom_to_token_index * atom_mask + n_token * torch.ones_like(
-        atom_to_token_index
-    ) * (1 - atom_mask)
+    atom_to_token_index = (
+        atom_to_token_index * atom_mask.int()
+        + n_token * torch.ones_like(atom_to_token_index) * (1 - atom_mask.int())
+    )
 
     # Prepare atom to token index for aggregation
     # Check for broadcasting and repeat accordingly