afebf69355cb7350922fedb4a9e46a046bb99a75
Vinay Swamy <swamyvinny@gmail.com>
2024-12-27 21:50:39 -0800

    add support for checking if the structure prediciton directory for a sample in s3 has a relaxed structure

--- scripts/data_preprocessing/create_protein_monomer_dataset_cache.py
+++ scripts/data_preprocessing/create_protein_monomer_dataset_cache.py
@@ -61,12 +61,34 @@ from openfold3.core.data.pipelines.preprocessing.dataset_cache import (
     help="The argument s3_client_config "
     "input as a JSON string with keys 'profile' and 'max_keys'.",
 )
+@click.option(
+    "--check_filename_exists",
+    required=False,
+    default=None,
+    type=str,
+    help="This enables a recursive search of the specified s3 directory"
+    "This is an expensive operation and should only be used when necessary."
+    "The passed filename should be a basename of the file to search for,"
+    "for exampl, 'best_structure_relaxed.pdb'; the associated ID of the "
+    "file will only be added if the file exists."
+    "You should specify a large value for --num_workers to speed up the search.",
+)
+@click.option(
+    "--num_workers",
+    required=False,
+    default=1,
+    type=int,
+    help="The number of workers to use for parallel processing."
+    "Only used if --target_filename is specified.",
+)
 def main(
     data_directory: Path,
     protein_reference_molecule_data_file: Path,
     dataset_name: str,
     output_path: Path,
     s3_client_config: dict | None = None,
+    check_filename_exists: str | None = None,
+    num_workers: int = 1,
 ):
     # Parse S3 config
     if s3_client_config is not None:
@@ -74,6 +96,7 @@ def main(
             s3_client_config = json.loads(s3_client_config)
         except json.JSONDecodeError:
             click.echo("Invalid max_seq_counts JSON string!")
+            return
     else:
         s3_client_config = {}
 
@@ -84,6 +107,8 @@ def main(
         dataset_name=dataset_name,
         output_path=output_path,
         s3_client_config=s3_client_config,
+        check_filename_exists=check_filename_exists,
+        num_workers=num_workers,
     )
 
 
