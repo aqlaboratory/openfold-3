e94dcd40f7bdf9ec44b5cdc46db4aacf27d54d3a
Vinay Swamy <swamyvinny@gmail.com>
2024-12-27 17:50:48 -0800

    formtting

--- scripts/make_monomer_distillation_set_datacache.py
+++ scripts/make_monomer_distillation_set_datacache.py
@@ -97,8 +97,8 @@ def get_sample_ids(bucket_name, prefix, profile):
         if "Contents" in page:
             for obj in page["Contents"]:
                 key = obj["Key"]
-                if key.endswith('best_structure_relaxed.pdb'):
-                # Extract sample_id from the key
+                if key.endswith("best_structure_relaxed.pdb"):
+                    # Extract sample_id from the key
                     parts = key.split("/")
                     if len(parts) > 2:  # Ensure the key has the structure
                         sample_id = parts[-2]
