Fixes to confidence losses to average over 5 samples

openfold3/core/loss/loss_utils.py
@@ -149,7 +149,7 @@ def compute_renamed_ground_truth(
 
 def loss_masked_batch_mean(
     loss: torch.tensor,
-    weight: float,
+    weight: torch.tensor,
     apply_weight: bool,
     nan_zero_weights: bool = False,
     eps: float = 1e-10,
@@ -168,10 +168,11 @@ def loss_masked_batch_mean(
         nan_zero_weights:
             Whether to set the loss to nan if the weight is zero
         eps:
-            Small value to avoid division by ze
+            Small value to avoid division by zero
     Returns:
         [1] Masked mean of loss for a batch
     """
+    weight = weight.expand_as(loss)
     mask = weight > 0
 
     if nan_zero_weights:

openfold3/core/utils/permutation_alignment.py
@@ -1761,7 +1761,12 @@ def safe_multi_chain_permutation_alignment(
             new_gt_features["atom_positions"] = torch.rand_like(
                 atom_positions_predicted
             )
-            new_gt_features["atom_resolved_mask"] = torch.ones_like(batch["atom_mask"])
+            atom_mask = batch["atom_mask"]
+            new_gt_features["atom_resolved_mask"] = torch.ones(
+                (*batch_dims, atom_mask.shape[-1]),
+                device=atom_mask.device,
+                dtype=atom_mask.dtype,
+            )
 
             # Disable all losses
             for loss_key, weights in batch["loss_weights"].items():