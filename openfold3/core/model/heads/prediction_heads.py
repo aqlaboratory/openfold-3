Bug fixes and moving confidence heads back to bf16 precision

openfold3/core/model/heads/prediction_heads.py
@@ -245,34 +245,32 @@ class PairformerEmbedding(nn.Module):
             ]
         )
 
-        in_dtype = si.dtype
-        with torch.amp.autocast(device_type="cuda", dtype=torch.bfloat16):
-            if pairformer_per_sample:
-                si, zij = self.per_sample_pairformer_emb(
-                    si=si,
-                    zij=zij,
-                    single_mask=single_mask,
-                    pair_mask=pair_mask,
-                    chunk_size=chunk_size,
-                    use_deepspeed_evo_attention=use_deepspeed_evo_attention,
-                    use_lma=use_lma,
-                    inplace_safe=inplace_safe,
-                    _mask_trans=_mask_trans,
-                )
-            else:
-                si, zij = self.pairformer_emb(
-                    si=si,
-                    zij=zij,
-                    single_mask=single_mask,
-                    pair_mask=pair_mask,
-                    chunk_size=chunk_size,
-                    use_deepspeed_evo_attention=use_deepspeed_evo_attention,
-                    use_lma=use_lma,
-                    inplace_safe=inplace_safe,
-                    _mask_trans=_mask_trans,
-                )
+        if pairformer_per_sample:
+            si, zij = self.per_sample_pairformer_emb(
+                si=si,
+                zij=zij,
+                single_mask=single_mask,
+                pair_mask=pair_mask,
+                chunk_size=chunk_size,
+                use_deepspeed_evo_attention=use_deepspeed_evo_attention,
+                use_lma=use_lma,
+                inplace_safe=inplace_safe,
+                _mask_trans=_mask_trans,
+            )
+        else:
+            si, zij = self.pairformer_emb(
+                si=si,
+                zij=zij,
+                single_mask=single_mask,
+                pair_mask=pair_mask,
+                chunk_size=chunk_size,
+                use_deepspeed_evo_attention=use_deepspeed_evo_attention,
+                use_lma=use_lma,
+                inplace_safe=inplace_safe,
+                _mask_trans=_mask_trans,
+            )
 
-        return si.to(dtype=in_dtype), zij.to(dtype=in_dtype)
+        return si, zij
 
 
 class PredictedAlignedErrorHead(nn.Module):

openfold3/core/model/layers/sequence_local_atom_attention.py
@@ -237,38 +237,43 @@ class NoisyPositionEmbedder(nn.Module):
 
         # Broadcast trunk single representation into atom single conditioning
         # [*, N_atom, c_atom]
-        sl_trunk = broadcast_token_feat_to_atoms(
+        si_trunk = broadcast_token_feat_to_atoms(
             token_mask=batch["token_mask"],
             num_atoms_per_token=batch["num_atoms_per_token"],
             token_feat=si_trunk,
             token_dim=-2,
         )
-        cl = cl + self.linear_s(self.layer_norm_s(sl_trunk))
+        cl = cl + self.linear_s(self.layer_norm_s(si_trunk))
 
         # Broadcast trunk pair representation into atom pair conditioning
-        # [*, N_atom, N_atom, c_atom_pair]
         zij_trunk = self.linear_z(self.layer_norm_z(zij_trunk))
-        zlj_trunk = broadcast_token_feat_to_atoms(
+
+        # z_lj: [*, N_atom, N_token, c_atom_pair]
+        zij_trunk = broadcast_token_feat_to_atoms(
             token_mask=batch["token_mask"],
             num_atoms_per_token=batch["num_atoms_per_token"],
             token_feat=zij_trunk,
             token_dim=-3,
         )
-        zlm_trunk = broadcast_token_feat_to_atoms(
+
+        # z_ml: [*, N_atom, N_atom, c_atom_pair]
+        zij_trunk = broadcast_token_feat_to_atoms(
             token_mask=batch["token_mask"],
             num_atoms_per_token=batch["num_atoms_per_token"],
-            token_feat=zlj_trunk.transpose(-2, -3),
+            token_feat=zij_trunk.transpose(-2, -3),
             token_dim=-3,
         )
-        zlm_trunk = zlm_trunk.transpose(-2, -3)
+
+        # z_lm: [*, N_atom, N_atom, c_atom_pair]
+        zij_trunk = zij_trunk.transpose(-2, -3)
 
         # [*, N_atom, N_atom, c_atom_pair] ->
         # [*, N_blocks, N_query, N_key, c_atom_pair]
-        zlm_trunk = convert_pair_rep_to_blocks(
-            plm=zlm_trunk, n_query=n_query, n_key=n_key
+        zij_trunk = convert_pair_rep_to_blocks(
+            plm=zij_trunk, n_query=n_query, n_key=n_key
         )
 
-        plm = plm + zlm_trunk
+        plm = plm + zij_trunk
 
         # Add noisy coordinate projection
         # [*, N_atom, c_atom]
@@ -462,6 +467,21 @@ class AtomAttentionEncoder(nn.Module):
                 n_query=self.n_query,
                 n_key=self.n_key,
             )
+
+        # TODO: Convert to this version for memory reasons.
+        #  Note that the l/m linear layers are reversed here, they should be renamed
+        #  Keeping it the same for now so that its compatible with previous checkpoints.
+        # cl_l, cl_m, atom_mask = convert_single_rep_to_blocks(
+        #     ql=cl,
+        #     n_query=self.n_query,
+        #     n_key=self.n_key,
+        #     atom_mask=batch["atom_mask"]
+        # )
+        #
+        # cl_lm = (
+        #     self.linear_m(self.relu(cl_l.unsqueeze(-2)))
+        #     + self.linear_l(self.relu(cl_m.unsqueeze(-3)))
+        # ) * atom_mask.unsqueeze(-1)
 
         # Add the combined single conditioning to the pair rep (line 13 - 14)
         cl_lm = self.linear_l(self.relu(cl.unsqueeze(-3))) + self.linear_m(

openfold3/projects/af3_all_atom/model.py
@@ -279,7 +279,10 @@ class AlphaFold3(nn.Module):
         )
 
         # Compute atom positions
-        with torch.no_grad():
+        with (
+            torch.no_grad(),
+            torch.amp.autocast(device_type="cuda", dtype=torch.float32),
+        ):
             noise_schedule = create_noise_schedule(
                 no_rollout_steps=no_rollout_steps,
                 **self.config.architecture.noise_schedule,
@@ -306,16 +309,21 @@ class AlphaFold3(nn.Module):
         }
 
         # Compute confidence logits
+        aux_heads_out = self.aux_heads(
+            batch=batch,
+            si_input=si_input,
+            output=output,
+            use_deepspeed_evo_attention=self.settings.use_deepspeed_evo_attention,
+            use_lma=self.settings.use_lma,
+            inplace_safe=inplace_safe,
+            _mask_trans=True,
+        )
+
         output.update(
-            self.aux_heads(
-                batch=batch,
-                si_input=si_input,
-                output=output,
-                use_deepspeed_evo_attention=self.settings.use_deepspeed_evo_attention,
-                use_lma=self.settings.use_lma,
-                inplace_safe=inplace_safe,
-                _mask_trans=True,
-            )
+            {
+                k: v.to(dtype=atom_positions_predicted.dtype)
+                for k, v in aux_heads_out.items()
+            }
         )
 
         return output
@@ -549,18 +557,18 @@ class AlphaFold3(nn.Module):
         batch = tensor_tree_map(lambda t: t.unsqueeze(1), batch)
         batch["ref_space_uid_to_perm"] = ref_space_uid_to_perm
 
+        # Mini rollout
+        rollout_output = self._rollout(
+            batch=batch,
+            si_input=si_input,
+            si_trunk=si_trunk,
+            zij_trunk=zij_trunk,
+            inplace_safe=inplace_safe,
+        )
+
+        output.update(rollout_output)
+
         with torch.amp.autocast(device_type="cuda", dtype=torch.float32):
-            # Mini rollout
-            rollout_output = self._rollout(
-                batch=batch,
-                si_input=si_input,
-                si_trunk=si_trunk,
-                zij_trunk=zij_trunk,
-                inplace_safe=inplace_safe,
-            )
-
-            output.update(rollout_output)
-
             # Apply permutation alignment for training and validation
             if "ground_truth" in batch:
                 # Update the ground-truth coordinates/mask in-place with the correct