0509ffd98bfe8846dfc38c4b85dab7db837f6947
Jennifer Wei <jennifer.wei@omsf.io>
2025-04-28 20:02:00 +0000

    Initial inference query json format commit

---
+++ openfold3/projects/af3_all_atom/config/inference_query_format.py
@@ -0,0 +1,69 @@
+import logging
+from typing import Annotated, Any, NamedTuple, Optional
+
+from pydantic import BaseModel, BeforeValidator, FilePath
+
+from openfold3.core.data.resources.residues import MoleculeType
+from openfold3.projects.af3_all_atom.config.dataset_configs import (
+    DirectoryPathOrNone,
+    FilePathOrNone,
+)
+
+
+def _ensure_list(value: Any) -> Any:
+    if not isinstance(value, list):
+        logging.info("Single value: {value} will be converted to a list")
+        return [value]
+    else:
+        return value
+
+
+def _convert_molecule_type(value: Any) -> Any:
+    if isinstance(value, MoleculeType):
+        return value
+    elif isinstance(value, str):
+        try:
+            return MoleculeType[value.upper()]
+        except KeyError:
+            logging.warning(
+                f"Found invalid {value=} for molecule type, skipping this example."
+            )
+            return None
+
+
+# Definition for Bonds
+class Atom(NamedTuple):
+    chain_id: str
+    residue_id: int
+    atom_id: int
+
+
+class Bond(NamedTuple):
+    atom1: Atom
+    atom2: Atom
+
+
+class Chain(BaseModel):
+    molecule_type: Annotated[MoleculeType, BeforeValidator(_convert_molecule_type)]
+    chain_ids: Annotated[list[str], BeforeValidator(_ensure_list)]
+    sequence: str
+    # Msa definition
+    use_msas: bool = True
+    unpaired_msa_file_paths: Optional[list[FilePath]] = None
+    paired_msa_file_path: Optional[FilePath] = None
+    # # Template definition
+    # use_templates: bool = False
+    # templates: ...
+    sdf_file_path: Optional[FilePath] = None
+
+
+class Query(BaseModel):
+    chains: list[Chain]
+    covalent_bonds: Optional[list[Bond]] = None
+
+
+class InferenceQuerySet(BaseModel):
+    seeds: list[int] = [42]
+    queries: list[Query]
+    ccd_file_path: FilePathOrNone
+    msa_directory_path: DirectoryPathOrNone
