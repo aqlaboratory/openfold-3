cb7d9653ab5245bc6e408f9601bdd89eeb1d891f
Jennifer Wei <jennifer.wei@omsf.io>
2025-05-16 04:48:57 +0000

    Move inference_query_format from projects directory to core.data.framework

---
+++ openfold3/core/config/path_definitions.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from typing import Annotated, Optional, Union
+
+from pydantic import (
+    BeforeValidator,
+    DirectoryPath,
+    FilePath,
+)
+
+
+def is_path_none(value: Optional[Union[str, Path]]) -> Optional[Path]:
+    if isinstance(value, Path):
+        return value
+    elif value is None or value.lower() in ["none", "null"]:
+        return None
+    else:
+        return Path(value)
+
+
+FilePathOrNone = Annotated[Optional[FilePath], BeforeValidator(is_path_none)]
+DirectoryPathOrNone = Annotated[Optional[DirectoryPath], BeforeValidator(is_path_none)]
--- openfold3/core/data/framework/inference_query_format.py
+++ openfold3/core/data/framework/inference_query_format.py
@@ -3,11 +3,11 @@ from typing import Annotated, Any, NamedTuple, Optional
 
 from pydantic import BaseModel, BeforeValidator, FilePath
 
-from openfold3.core.data.resources.residues import MoleculeType
-from openfold3.projects.af3_all_atom.config.dataset_configs import (
+from openfold3.core.config.path_definitions import (
     DirectoryPathOrNone,
     FilePathOrNone,
 )
+from openfold3.core.data.resources.residues import MoleculeType
 
 
 def _ensure_list(value: Any) -> Any:
