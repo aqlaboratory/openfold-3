0688995fec6c2cf0959711b7186fbfaa523b6027
Jennifer Wei <jennifer.wei@omsf.io>
2025-05-21 10:24:04 +0000

    Draft changes to ExperimentRunner (fka ExperimentBuilder)

--- openfold3/entry_points/trainable_experiment/__init__.py
+++
@@ -1,4 +0,0 @@
-from .builder import TrainableExperimentBuilder
-from .validator import TrainableExperimentConfig
-
-__all__ = ["TrainableExperimentBuilder", "TrainableExperimentConfig"]
--- openfold3/entry_points/validator.py
+++ openfold3/entry_points/validator.py
@@ -2,6 +2,14 @@ from typing import Any, Optional
 
 from pydantic import BaseModel
 
+from openfold3.core.config.path_definitions import FilePathOrNone
+from openfold3.projects.af3_all_atom.config.dataset_configs import (
+    InferenceDatasetSpec,
+    TrainingDatasetPaths,
+    TrainingDatasetSpec,
+)
+from openfold3.projects.af3_all_atom.project_entry import ModelUpdate
+
 
 class CheckpointConfig(BaseModel):
     every_n_epochs: int
@@ -12,11 +20,17 @@ class CheckpointConfig(BaseModel):
 
 class WandbConfig(BaseModel):
     project: str
+    experiment_name: str
     entity: Optional[str] = None
     group: Optional[str] = None
     id: Optional[str] = None
-    experiment_name: str
     offline: bool = False
+
+
+class LoggingConfig(BaseModel):
+    log_lr: bool = True
+    checkpoint_config: CheckpointConfig | None = None
+    wandb_config: WandbConfig | None = None
 
 
 class DataModuleArgs(BaseModel):
@@ -58,25 +72,40 @@ class DataModuleArgs(BaseModel):
             num_epochs=self.num_epochs,
         )
 
-class TrainableExperimentConfig(BaseModel):
-    mode: str
-    project_type: str
-    presets: list[str]
-    mpi_plugin: bool
-    seed: int
-    batch_size: int
-    num_gpus: int
-    output_dir: str
-    log_lr: bool
-    compile: bool
-    num_workers: int
-    epoch_len: int
-    restart_checkpoint_path: str
-    deepspeed_config_path: str | None = None
-    checkpoint: CheckpointConfig | None = None
-    wandb: WandbConfig | None = None
 
-    pl_trainer: dict[str, Any]
-    dataset_paths: dict[str, Any]
-    config_update: dict[str, Any]
+class PlTrainerArgs(BaseModel):
+    """Arguments for openfold3.core.pl_trainer.Trainer"""
+
+    max_epochs: int = 1000  # pl_trainer default
+    accelerator: str = "gpu"
+    precision: int = 16
+    num_nodes: int = 1
+    profiler: Optional[str] = None
+    log_every_n_steps: int = 1
+    enable_checkpointing: bool = True
+    enable_model_summary: bool = False
+
+
+class ExperimentConfig(BaseModel):
+    mode: str
+    output_dir: str
+    mpi_plugin: bool = False
+    deepspeed_config_path: str | None = None
+    compile: bool = False
+    num_gpus: int = 1
+
+    pl_trainer_args: PlTrainerArgs
+    model_update: ModelUpdate
+
+
+class TrainableExperimentConfig(ExperimentConfig):
+    seed: int = 42
+    data_seed: int = 1234
+    num_workers: int = 0
+    epoch_len: int = 2
+    restart_checkpoint_path: FilePathOrNone = None 
+
+    logging_config: LoggingConfig | None = None
+    dataset_paths: dict[str, TrainingDatasetPaths]
+    model_update: ModelUpdate = ModelUpdate(presets=["train"])
     dataset_configs: dict[str, Any]
