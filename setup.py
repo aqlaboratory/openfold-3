3b64cde60951278bddc30ef1706229f3fd1b29b7
David W.H. Swenson <david.swenson@omsf.io>
2025-06-06 22:26:55 +0000

    start to packaging

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import ctypes
 import os
 import subprocess
 
@@ -23,8 +24,6 @@ from torch.utils.cpp_extension import (
     CppExtension,
     CUDAExtension,
 )
-
-from scripts.utils import get_nvidia_cc
 
 version_dependent_macros = [
     "-DVERSION_GE_1_1",
@@ -41,6 +40,73 @@ extra_cuda_flags = [
     "--expt-extended-lambda",
 ]
 
+def get_nvidia_cc():
+    """
+    Returns a tuple containing the Compute Capability of the first GPU
+    installed in the system (formatted as a tuple of strings) and an error
+    message. When the former is provided, the latter is None, and vice versa.
+
+    Adapted from script by Jan Schlüte t
+    https://gist.github.com/f0k/63a664160d016a491b2cbea15913d549
+    """
+    CUDA_SUCCESS = 0
+
+    libnames = [
+        "libcuda.so",
+        "libcuda.dylib",
+        "cuda.dll",
+        "/usr/local/cuda/compat/libcuda.so",  # For Docker
+    ]
+    for libname in libnames:
+        try:
+            cuda = ctypes.CDLL(libname)
+        except OSError:
+            continue
+        else:
+            break
+    else:
+        return None, "Could not load any of: " + " ".join(libnames)
+
+    nGpus = ctypes.c_int()
+    cc_major = ctypes.c_int()
+    cc_minor = ctypes.c_int()
+
+    result = ctypes.c_int()
+    device = ctypes.c_int()
+    error_str = ctypes.c_char_p()
+
+    result = cuda.cuInit(0)
+    if result != CUDA_SUCCESS:
+        cuda.cuGetErrorString(result, ctypes.byref(error_str))
+        if error_str.value:
+            return None, error_str.value.decode()
+        else:
+            return None, f"Unknown error: cuInit returned {result}"
+    result = cuda.cuDeviceGetCount(ctypes.byref(nGpus))
+    if result != CUDA_SUCCESS:
+        cuda.cuGetErrorString(result, ctypes.byref(error_str))
+        return None, error_str.value.decode()
+
+    if nGpus.value < 1:
+        return None, "No GPUs detected"
+
+    result = cuda.cuDeviceGet(ctypes.byref(device), 0)
+    if result != CUDA_SUCCESS:
+        cuda.cuGetErrorString(result, ctypes.byref(error_str))
+        return None, error_str.value.decode()
+
+    if (
+        cuda.cuDeviceComputeCapability(
+            ctypes.byref(cc_major), ctypes.byref(cc_minor), device
+        )
+        != CUDA_SUCCESS
+    ):
+        return None, "Compute Capability not found"
+
+    major = cc_major.value
+    minor = cc_minor.value
+
+    return (major, minor), None
 
 def get_cuda_bare_metal_version(cuda_dir):
     if cuda_dir is None or torch.version.cuda is None:
@@ -127,9 +193,6 @@ else:
     ]
 
 setup(
-    name="openfold3",
-    version="0.1.0",
-    description="A PyTorch reimplementation of DeepMind's AlphaFold 2 & 3",
     author="OpenFold Team",
     author_email="jennifer.wei@omsf.io",
     license="Apache License, Version 2.0",
