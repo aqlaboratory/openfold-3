3499e4fe38554f56c21c4420090a927a4ad3e79c
Gergo Nikolenyi <47091330+gnikolenyi@users.noreply.github.com>
2025-08-06 09:38:35 +0000

    Update template parser tests.

--- tests/test_template_parsers.py
+++ tests/test_template_parsers.py
@@ -84,7 +84,7 @@ def test_m8_parser():
 
     m8_parser = M8Parser(max_sequences=max_sequences)
     m8_cigar = pd.read_csv(file_path, sep="\t", header=None)
-    templates = m8_parser(m8_cigar)
+    templates = m8_parser(m8_cigar, query_seq_str=QUERY_SEQUENCE)
     expected_templates = np.load(output_path, allow_pickle=True)["templates"].item()
     assert len(templates) == len(expected_templates)
     for actual, expected in zip(templates.values(), expected_templates.values()):
@@ -92,7 +92,7 @@ def test_m8_parser():
 
     m8_no_cigar = m8_cigar.loc[:, m8_cigar.columns != "cigar"].copy()
     output_path_no_cigar = TEST_DIR / "outputs/m8_no_cigar.npz"
-    templates_no_cigar = m8_parser(m8_no_cigar)
+    templates_no_cigar = m8_parser(m8_no_cigar, query_seq_str=QUERY_SEQUENCE)
     expected_templates_no_cigar = np.load(output_path_no_cigar, allow_pickle=True)[
         "templates"
     ].item()
