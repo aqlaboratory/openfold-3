61b72b202dbff666354c44132f24ed89474d6ce9
Jennifer Wei <jennifer.wei@omsf.io>
2025-10-24 09:48:36 +0000

    Update init to include entry_points and run_openfold.py

--- openfold3/__init__.py
+++ openfold3/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__all__ = ["core", "projects"]
+__all__ = ["core", "projects", "entry_points", "run_openfold"]
 
 import importlib.util
 from . import hacks
