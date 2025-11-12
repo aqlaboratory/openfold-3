f9e84c984f632a16e7328115e3d7cd6609ce8fc4
Santi Villalba <sdvillal@gmail.com>
2025-11-12 22:50:29 +0100

    Remove deepspeed hacks for good

--- openfold3/__init__.py
+++ openfold3/__init__.py
@@ -19,8 +19,6 @@ import importlib.util
 import gemmi
 from packaging import version
 
-from . import hacks  # noqa: F401
-
 if version.parse(gemmi.__version__) >= version.parse("0.7.3"):
     gemmi.set_leak_warnings(False)
 
@@ -32,3 +30,4 @@ if importlib.util.find_spec("deepspeed") is not None:
     # This has weird effects with hanging if libaio is not installed and can
     # cause restart errors if run is preempted in the middle of autotuning
     deepspeed.HAS_TRITON = False
+    # FIXME: do we need this? it is really invasive with other potential users of DS
