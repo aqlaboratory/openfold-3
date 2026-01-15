fea06b832e305bae252371e577c73cec80973cc0
Jan Domanski <jandom@gmail.com>
2026-01-15 10:15:15 +0100

    found the old test_labels.py and moved it

--- openfold3/tests/core/data/primitives/structure/test_labels.py
+++ openfold3/tests/core/data/primitives/structure/test_labels.py
@@ -14,8 +14,55 @@
 
 import numpy as np
 import pytest
+from biotite.structure import Atom, AtomArray, array
 
-from openfold3.core.data.primitives.structure.labels import assign_atom_indices
+from openfold3.core.data.primitives.structure.labels import (
+    AtomArrayView,
+    assign_atom_indices,
+    residue_view_iter,
+)
+from openfold3.tests.custom_assert_utils import assert_atomarray_equal
+
+
+@pytest.fixture
+def fake_atom_array() -> AtomArray:
+    atom1 = Atom([1, 2, 3], chain_id="A")
+    atom2 = Atom([2, 3, 4], chain_id="A")
+    atom3 = Atom([3, 4, 5], chain_id="B")
+    atom4 = Atom([3, 4, 5], chain_id="B")
+    return array([atom1, atom2, atom3, atom4])
+
+
+class TestAtomArrayView:
+    """Tests for the AtomArrayView class."""
+
+    def test_atom_array_slice_view(self, fake_atom_array):
+        slice_indices = slice(2, 4, 1)
+        slice_view = AtomArrayView(fake_atom_array, slice_indices)
+
+        # This slice view has an underlying base
+        assert slice_view.chain_id.base is not None
+        assert len(slice_view) == 2
+        np.testing.assert_equal(slice_view.chain_id, np.array(["B", "B"]))
+
+        # If we materialize, we expect a new array
+        materialized = slice_view.materialize()
+        assert isinstance(materialized, AtomArray)
+        assert_atomarray_equal(materialized, fake_atom_array[slice_indices])
+
+    def test_atom_array_mask_view(self, fake_atom_array):
+        mask_indices = np.array([False, True, False, True])
+        mask_view = AtomArrayView(fake_atom_array, mask_indices)
+
+        # When the index used is not basic indexing, we get new arrays
+        assert mask_view.chain_id.base is None
+        assert len(mask_view) == 2
+        np.testing.assert_equal(mask_view.chain_id, np.array(["A", "B"]))
+
+        # If we materialize, we expect a new array
+        materialized = mask_view.materialize()
+        assert isinstance(materialized, AtomArray)
+        assert_atomarray_equal(materialized, fake_atom_array[mask_indices])
 
 
 class TestAssignAtomIndices:
@@ -55,3 +102,43 @@ class TestAssignAtomIndices:
         assert np.array_equal(
             dummy_atom_array._atom_idx, np.arange(len(dummy_atom_array))
         )
+
+
+class TestResidueViewIter:
+    """Tests for the residue_view_iter function."""
+
+    def test_yields_correct_number_of_residues(self, mse_ala_atom_array):
+        """Yields one view per residue in the AtomArray."""
+        residue_views = list(residue_view_iter(mse_ala_atom_array))
+
+        # mse_ala_atom_array has 2 residues: MSE (res_id=1) and ALA (res_id=2)
+        assert len(residue_views) == 2
+
+    def test_yields_atom_array_views(self, mse_ala_atom_array):
+        """Each yielded item is an AtomArrayView."""
+        for view in residue_view_iter(mse_ala_atom_array):
+            assert isinstance(view, AtomArrayView)
+
+    def test_each_view_contains_correct_atoms(self, mse_ala_atom_array):
+        """Each view contains only atoms from one residue."""
+        residue_views = list(residue_view_iter(mse_ala_atom_array))
+
+        # First residue is MSE with 8 atoms
+        mse_view = residue_views[0]
+        assert len(mse_view) == 8
+        assert np.all(mse_view.res_name == "MSE")
+        assert np.all(mse_view.res_id == 1)
+
+        # Second residue is ALA with 5 atoms
+        ala_view = residue_views[1]
+        assert len(ala_view) == 5
+        assert np.all(ala_view.res_name == "ALA")
+        assert np.all(ala_view.res_id == 2)
+
+    def test_empty_atom_array_yields_nothing(self):
+        """An empty AtomArray yields no residue views."""
+        empty_array = AtomArray(0)
+
+        residue_views = list(residue_view_iter(empty_array))
+
+        assert len(residue_views) == 0
