b306f95c198a066808d84b864911f079fb008758
Jan Domanski <jan.domanski@omsf.io>
2026-02-10 13:29:19 +0000

    linter fixes

--- openfold3/tests/test_setup_openfold.py
+++ openfold3/tests/test_setup_openfold.py
@@ -5,7 +5,7 @@ def test_setup_biotite_ccd(tmp_path):
     ccd_path = tmp_path / "test_ccd.cif"
     has_downloaded = setup_biotite_ccd(ccd_path=ccd_path, force_download=False)
     assert ccd_path.exists()
-    assert has_downloaded == True
+    assert has_downloaded
 
     has_downloaded = setup_biotite_ccd(ccd_path=ccd_path, force_download=False)
-    assert has_downloaded == False
+    assert not has_downloaded
