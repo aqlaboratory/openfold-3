080d0d0ec2cac3894ff8826267791791643a4b08
Jan Domanski <jan.domanski@omsf.io>
2026-02-10 21:52:05 +0000

    run a linter

--- openfold3/tests/core/data/pipelines/preprocessing/test_template.py
+++ openfold3/tests/core/data/pipelines/preprocessing/test_template.py
@@ -1,35 +1,23 @@
+from pathlib import Path
+
+from biotite.database.rcsb import fetch
+
 from openfold3.core.data.io.sequence.template import (
     A3mParser,
     parse_template_alignment,
 )
-import pytest
 from openfold3.core.data.io.structure.cif import _load_ciffile
-
-from biotite.database.rcsb import fetch
-
 from openfold3.core.data.primitives.structure.metadata import (
-    get_chain_to_canonical_seq_dict,
-    get_cif_block,
-    
-)
-
-from openfold3.core.data.io.sequence.template import (
-A3mParser
-)
-from openfold3.core.data.primitives.structure.metadata import (
-get_asym_id_to_canonical_seq_dict,
-get_label_to_author_chain_id_dict,
+    get_asym_id_to_canonical_seq_dict,
+    get_label_to_author_chain_id_dict,
 )
 
 
-from pathlib import Path
-
-class TestTemplatePreprocessor():
-
+class TestTemplatePreprocessor:
     def test_template_has_author_chain_id(self, tmp_path):
         """
         https://github.com/aqlaboratory/openfold-3/issues/101
-        
+
         """
 
         alignment_file = Path(__file__).parent / "colabfold_template.m8"
@@ -37,10 +25,9 @@ class TestTemplatePreprocessor():
         templates = parse_template_alignment(
             aln_path=Path(alignment_file),
             query_seq_str=query_seq_str,
-            max_sequences=200
-
+            max_sequences=200,
         )
-        
+
         # find the offending "1rnb_A"
         template = templates[16]
         assert template.chain_id == "A" and template.entry_id == "1rnb"
@@ -50,7 +37,6 @@ class TestTemplatePreprocessor():
             format="cif",
             target_path=tmp_path,
         )
-
 
         template_structure_file = tmp_path / f"{template.entry_id}.cif"
 
@@ -67,17 +53,16 @@ class TestTemplatePreprocessor():
 
         parser = A3mParser(max_sequences=None)
         parsed = parser(
-                (
-                    f">query_X/1-{len(query_seq_str)}\n"
-                    f"{query_seq_str}\n"
-                    f">{template.entry_id}_{label_chain_id}/{1}-{len(template_sequence)}\n"
-                    f"{template_sequence}\n"
-                ),
-                query_seq_str,
-                realign=True,
-            )
-            
+            (
+                f">query_X/1-{len(query_seq_str)}\n"
+                f"{query_seq_str}\n"
+                f">{template.entry_id}_{label_chain_id}/{1}-{len(template_sequence)}\n"
+                f"{template_sequence}\n"
+            ),
+            query_seq_str,
+            realign=True,
+        )
+
         assert len(parsed) == 2
         assert parsed[0].seq_id == 1
         assert parsed[1].seq_id < 1
-        
