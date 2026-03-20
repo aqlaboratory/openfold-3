9001b4d3bff78680df57e76c79d02ab488b026ee
gnikolenyi <nikolenyi.gergo@gmail.com>
2026-03-19 22:31:24 -0400

    P1-20. MSA Pairing Code Broken

--- openfold3/core/data/primitives/sequence/msa.py
+++ openfold3/core/data/primitives/sequence/msa.py
@@ -636,10 +636,11 @@ def extract_alignments_to_pair(
         for msa_name in msas_to_pair:
             m = rep_msa_map_per_chain.get(msa_name)
             if m is not None:
-                # exclude query from subsequent MSAs
+                # exclude query from subsequent MSAs 
+                # only relevant when there are multiple MSAs to pair
                 if len(msa_arrays_to_pair_i) > 0:
-                    non_query_mask = np.bool([1] * len(m.msa.shape[0]))
-                    len(m.msa.shape[0])[0] = False
+                    non_query_mask = np.ones(m.msa.shape[0], dtype=bool)
+                    non_query_mask[0] = False
                     m = m.subset(non_query_mask)
                 if len(m) > 1:
                     msa_arrays_to_pair_i.append(m)
