fe85b03bf8cfe8befb1f67ebaef3d57d743b15cb
gnikolenyi <nikolenyi.gergo@gmail.com>
2026-03-20 15:14:27 -0400

    P2-36. AB/AG Type Features Not Permuted

--- openfold3/core/utils/permutation_alignment.py
+++ openfold3/core/utils/permutation_alignment.py
@@ -1319,6 +1319,24 @@ def update_gt_position_features(
         inter_filter_atomized = inter_filter_atomized.transpose(-1, -2)
         updated_ground_truth_features["inter_filter_atomized"] = inter_filter_atomized
 
+    intra_ab_ag_type_atomized = ground_truth_features.get("intra_ab_ag_type_atomized")
+    if intra_ab_ag_type_atomized is not None:
+        intra_ab_ag_type_atomized = permute_gt_atom_features(
+            [intra_ab_ag_type_atomized], gt_atom_indexes
+        )[0]
+        updated_ground_truth_features["intra_ab_ag_type_atomized"] = intra_ab_ag_type_atomized
+
+    inter_ab_ag_type_atomized = ground_truth_features.get("inter_ab_ag_type_atomized")
+    if inter_ab_ag_type_atomized is not None:
+        inter_ab_ag_type_atomized = permute_gt_atom_features(
+            [inter_ab_ag_type_atomized], gt_atom_indexes
+        )[0]
+        inter_ab_ag_type_atomized = permute_gt_atom_features(
+            [inter_ab_ag_type_atomized.transpose(-1, -2)], gt_atom_indexes
+        )[0]
+        inter_ab_ag_type_atomized = inter_ab_ag_type_atomized.transpose(-1, -2)
+        updated_ground_truth_features["inter_ab_ag_type_atomized"] = inter_ab_ag_type_atomized
+
     return updated_ground_truth_features
 
 
