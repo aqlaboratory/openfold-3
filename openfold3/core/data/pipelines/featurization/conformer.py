819d5422e1fd7b7af2a8af5457fe517cc4f3f952
etowahadams <etowahadams@gmail.com>
2026-04-29 19:03:00 -0400

    fix: prevent from segfault

--- openfold3/core/data/pipelines/featurization/conformer.py
+++ openfold3/core/data/pipelines/featurization/conformer.py
@@ -107,12 +107,9 @@ def featurize_reference_conformers_of3(
             # Intermediate reference coordinates (without random rotation & translation)
             coords = conf.GetAtomPosition(atom.GetIdx())
             mol_ref_mask.append(int(atom.GetBoolProp("annot_used_atom_mask")))
-            mol_ref_pos.append(coords)
-            # Some PyPI installations crash here due to
-            # ABI mismatch between RDKit and PyTorch
-            # Leaving a quick fix commented (beware, moving into slow python land)
-            # Remove if nobody else hits the problem
-            # mol_ref_pos.append([coords.x, coords.y, coords.z])
+            # Avoid passing RDKit Point3D objects directly into torch.tensor(),
+            # which can segfault in some RDKit/PyTorch builds.
+            mol_ref_pos.append((coords.x, coords.y, coords.z))
 
             # Atom elements (0-indexed)
             element_symbol = atom.GetSymbol()
