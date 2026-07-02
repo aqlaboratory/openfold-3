d980792601a092f2788de6452c2a99b448dc2a07
ioannisa92 <yianni.anastopoulos@unnaturalproducts.com>
2026-07-02 09:47:45 -0700

    format:

--- openfold3/tests/test_relpos.py
+++ openfold3/tests/test_relpos.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torch
 import pytest
+import torch
 
 from openfold3.core.utils.relpos import cyclic_offset, relpos_complex
 
