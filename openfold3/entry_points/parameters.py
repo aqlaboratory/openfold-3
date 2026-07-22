f5dd90917840950a4daa0a9824578b158d8d642b
jnwei <jennifer.wei@omsf.io>
2026-07-22 18:00:50 +0700

    update parameter name with temp ob parameter name

--- openfold3/entry_points/parameters.py
+++ openfold3/entry_points/parameters.py
@@ -37,7 +37,7 @@ class CheckpointEntry:
 
 OPENFOLD_MODEL_CHECKPOINT_REGISTRY = {
     "openfold3-ob-test": CheckpointEntry(
-        file_name="test-ob-v1.pt", version_compatibility=">=0.5"
+        file_name="ob-rc-v1.pt", version_compatibility=">=0.5"
     ),
     "openfold3-p2-145k": CheckpointEntry(
         file_name="of3-p2-145k.pt", version_compatibility=">=0.4,<0.5"
